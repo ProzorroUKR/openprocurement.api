3d4eec43bd7945f407515cb8616ed7f461e473b0
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2015-11-23 15:02:13 +0200

    added base tests configuration

---
+++ openprocurement/tender/openua/tests/base.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import os
+import webtest
+from openprocurement.api.tests.base import (test_tender_data,
+                                            BaseTenderWebTest,
+                                            PrefixedRequestClass)
+
+
+test_tender_ua_data = test_tender_data.copy()
+test_tender_ua_data['subtype'] = "TenderUA"
+test_tender_ua_data['magicUnicorns'] = 15
+
+
+class BaseTenderUAWebTest(BaseTenderWebTest):
+    initial_data = test_tender_ua_data
+    initial_status = None
+    initial_bids = None
+    initial_lots = None
+
+    def setUp(self):
+        self.app = webtest.TestApp(
+            "config:tests.ini", relative_to=os.path.dirname(__file__))
+        self.app.RequestClass = PrefixedRequestClass
+        self.app.authorization = ('Basic', ('token', ''))
+        self.couchdb_server = self.app.app.registry.couchdb_server
+        self.db = self.app.app.registry.db
+
+    def tearDown(self):
+        del self.couchdb_server[self.db.name]
+
