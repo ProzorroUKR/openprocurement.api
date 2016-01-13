f3b252ad923b052cbc3bb1c6997926d92e831afb
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2016-01-13 18:53:04 +0200

    include all tests

--- openprocurement/tender/limited/tests/main.py
+++ openprocurement/tender/limited/tests/main.py
@@ -2,12 +2,17 @@
 
 import unittest
 
-from openprocurement.tender.limited.tests import tender
+from openprocurement.tender.limited.tests import (tender, award, contract,
+                                                  document, cancellation)
 
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(tender.suite())
+    suite.addTest(award.suite())
+    suite.addTest(document.suite())
+    suite.addTest(contract.suite())
+    suite.addTest(cancellation.suite())
     return suite
 
 
