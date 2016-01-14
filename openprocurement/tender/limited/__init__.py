d57c2ccb92855c8d25b2e58fc56164ae8abd5f30
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2016-01-14 13:51:19 +0200

    fixed module scan

--- openprocurement/tender/limited/__init__.py
+++ openprocurement/tender/limited/__init__.py
@@ -3,4 +3,4 @@ from openprocurement.tender.limited.models import Tender
 
 def includeme(config):
     config.add_tender_procurementMethodType(Tender)
-    config.scan("openprocurement.tender.limited")
+    config.scan("openprocurement.tender.limited.views")
