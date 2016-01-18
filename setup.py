53de93458d801a316b096319a670443f80ec5891
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2016-01-18 10:02:39 +0200

    updated setup definitions

--- setup.py
+++ setup.py
@@ -3,30 +3,47 @@ import os
 
 version = '1.0'
 
+requires = [
+    'setuptools',
+    'openprocurement.api',
+]
+
+test_requires = requires + [
+    'webtest',
+    'python-coveralls',
+]
+
+entry_points = {
+    'openprocurement.api.plugins': [
+        'reporting = openprocurement.tender.limited:includeme'
+    ]
+}
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+
 setup(name='openprocurement.tender.openeu',
       version=version,
       description="",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=README,
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
+        "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
         ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
-      license='GPL',
+      keywords="web services",
+      author='Quintagroup, Ltd.',
+      author_email='info@quintagroup.com',
+      url='https://github.com/openprocurement/openprocurement.tender.openeu',
+      license='Apache License 2.0',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['openprocurement', 'openprocurement.tender'],
       include_package_data=True,
       zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+      install_requires=requires,
+      extras_require={'test': test_requires},
+      test_suite="openprocurement.tender.openeu.tests.main.suite",
+      entry_points=entry_points)
