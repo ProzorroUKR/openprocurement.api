048825097329b33987697eb373aeb8cb44dbf8f6
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2016-01-18 15:28:34 +0200

    updated tender model

--- openprocurement/tender/openeu/models.py
+++ openprocurement/tender/openeu/models.py
@@ -1,5 +1,12 @@
+from zope.interface import implementer
+from openprocurement.api.models import ITender
 from openprocurement.api.models import Tender as BaseTender
+from schematics.types import StringType
 
 
+@implementer(ITender)
 class Tender(BaseTender):
     """ OpenEU tender model """
+
+    procurementMethodType = StringType(default="aboveThresholdEU")
+    title_en = StringType(required=True)
