8b9bbf6424d6076ba1215ad6388908f472a44a42
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2016-01-18 09:59:46 +0200

    added tests structure

---
+++ openprocurement/tender/openeu/tests/main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+
+def suite():
+    suite = unittest.TestSuite()
+    return suite
+
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
