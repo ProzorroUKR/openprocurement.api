64ea2db0e60896c62ff10ecb1b4a6f2366a1ad9a
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2016-01-23 14:45:14 +0200

    added qualification patch data validation

---
+++ openprocurement/tender/openeu/validation.py
@@ -0,0 +1,6 @@
+from openprocurement.api.validation import validate_data
+from openprocurement.tender.openeu.models import Qualification
+
+
+def validate_patch_qualification_data(request):
+    return validate_data(request, Qualification, True)
