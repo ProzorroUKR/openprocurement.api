ae634c67ef02829b58af0feea24e79f9e527b163
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2016-01-26 11:21:18 +0200

    updated roles, validation, status

--- openprocurement/tender/openeu/models.py
+++ openprocurement/tender/openeu/models.py
@@ -2,7 +2,7 @@ from uuid import uuid4
 from zope.interface import implementer
 from schematics.types import StringType, MD5Type
 from schematics.types.compound import ModelType
-from schematics.transforms import blacklist
+from schematics.transforms import blacklist, whitelist
 from schematics.exceptions import ValidationError
 from openprocurement.api.models import ITender
 from openprocurement.api.models import (Document, Model, Address, Period,
@@ -13,9 +13,13 @@ from openprocurement.api.models import Item as BaseItem
 from openprocurement.api.models import Bid as BaseBid
 from openprocurement.api.models import Award as BaseAward
 from openprocurement.api.models import ContactPoint as BaseContactPoint
-from openprocurement.api.models import (validate_cpv_group, validate_items_uniq,
-                                        schematics_embedded_role,
-                                        schematics_default_role)
+from openprocurement.api.models import validate_cpv_group, validate_items_uniq
+from openprocurement.api.models import (
+    plain_role, create_role, edit_role, cancel_role, view_role, listing_role,
+    auction_view_role, auction_post_role, auction_patch_role, enquiries_role,
+    auction_role, chronograph_role, chronograph_view_role, view_bid_role,
+    Administrator_bid_role, Administrator_role, schematics_default_role,
+    schematics_embedded_role)
 
 
 class Item(BaseItem):
@@ -54,6 +58,27 @@ class Organization(Model):
 
 
 class Bid(BaseBid):
+    class Options:
+        roles = {
+            'Administrator': Administrator_bid_role,
+            'embedded': view_bid_role,
+            'view': view_bid_role,
+            'create': whitelist('value', 'tenderers', 'parameters', 'lotValues'),
+            'edit': whitelist('value', 'tenderers', 'parameters', 'lotValues'),
+            'auction_view': whitelist('value', 'lotValues', 'id', 'date', 'parameters', 'participationUrl'),
+            'auction_post': whitelist('value', 'lotValues', 'id', 'date'),
+            'auction_patch': whitelist('id', 'lotValues', 'participationUrl'),
+            'active.enquiries': whitelist(),
+            'active.tendering': whitelist(),
+            'active.pre-qualification': enquiries_role,
+            'active.pre-qualification.stand-still': enquiries_role,
+            'active.auction': whitelist(),
+            'active.qualification': view_bid_role,
+            'active.awarded': view_bid_role,
+            'complete': view_bid_role,
+            'unsuccessful': view_bid_role,
+            'cancelled': view_bid_role,
+        }
     status = StringType(choices=['pending', 'active', 'invalid', 'deleted'],
                         default='pending')
 
@@ -87,6 +112,39 @@ class Qualification(Model):
 @implementer(ITender)
 class Tender(BaseTender):
     """ OpenEU tender model """
+    class Options:
+        roles = {
+            'plain': plain_role,
+            'create': create_role,
+            'edit': edit_role,
+            'edit_active.enquiries': edit_role,
+            'edit_active.tendering': cancel_role,
+            'edit_active.auction': cancel_role,
+            'edit_active.qualification': cancel_role,
+            'edit_active.awarded': cancel_role,
+            'edit_complete': whitelist(),
+            'edit_unsuccessful': whitelist(),
+            'edit_cancelled': whitelist(),
+            'view': view_role,
+            'listing': listing_role,
+            'auction_view': auction_view_role,
+            'auction_post': auction_post_role,
+            'auction_patch': auction_patch_role,
+            'active.enquiries': enquiries_role,
+            'active.tendering': enquiries_role,
+            'active.pre-qualification': enquiries_role,
+            'active.pre-qualification.stand-still': enquiries_role,
+            'active.auction': auction_role,
+            'active.qualification': view_role,
+            'active.awarded': view_role,
+            'complete': view_role,
+            'unsuccessful': view_role,
+            'cancelled': view_role,
+            'chronograph': chronograph_role,
+            'chronograph_view': chronograph_view_role,
+            'Administrator': Administrator_role,
+            'default': schematics_default_role,
+        }
 
     procurementMethodType = StringType(default="aboveThresholdEU")
     title_en = StringType(required=True, min_length=1)
@@ -97,6 +155,8 @@ class Tender(BaseTender):
     bids = ListType(ModelType(Bid), default=list())  # A list of all the companies who entered submissions for the tender.
     qualifications = ListType(ModelType(Qualification), default=list())
     qualificationPeriod = ModelType(Period)
-    # status = StringType(choices=['active.tendering', 'active.pre-qualification', 'active.pre-qualification.stand-still', 'active.auction',
-                                  # 'active.awarded', 'complete', 'cancelled', 'unsuccessful'], default='active.tendering')
+    status = StringType(choices=['active.enquiries', 'active.tendering', 'active.pre-qualification', 'active.pre-qualification.stand-still', 'active.auction',
+                                  'active.awarded', 'complete', 'cancelled', 'unsuccessful'], default='active.enquiries')
 
+    def validate_tenderPeriod(self, data, period):
+        return
