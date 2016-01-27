4b2919eac1249f48dc706e4e7d68f01e8f26e780
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2016-01-27 17:30:08 +0200

    added qualification traversal support

---
+++ openprocurement/tender/openeu/traversal.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from openprocurement.api.traversal import Root, get_item
+
+
+def qualifications_factory(request):
+    request.validated['tender_src'] = {}
+    root = Root(request)
+    if not request.matchdict or not request.matchdict.get('tender_id'):
+        return root
+    request.validated['tender_id'] = request.matchdict['tender_id']
+    tender = request.tender
+    tender.__parent__ = root
+    request.validated['tender'] = tender
+    request.validated['tender_status'] = tender.status
+    if request.method != 'GET':
+        request.validated['tender_src'] = tender.serialize('plain')
+    if request.matchdict.get('qualification_id'):
+        qualification = get_item(tender, 'qualification', request)
+        if request.matchdict.get('document_id'):
+            return get_item(qualification, 'document', request)
+        else:
+            return qualification
+    request.validated['id'] = request.matchdict['tender_id']
+    return tender
