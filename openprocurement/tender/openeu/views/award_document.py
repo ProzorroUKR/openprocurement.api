ee7d355c3b5dc409ce9070b446ef5b7f618d29a6
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2016-01-29 17:55:26 +0200

    added award document resource

---
+++ openprocurement/tender/openeu/views/award_document.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from logging import getLogger
+from openprocurement.api.views.award_document import TenderAwardDocumentResource as BaseResource
+from openprocurement.api.utils import opresource
+
+LOGGER = getLogger(__name__)
+
+
+@opresource(name='Tender EU Award Documents',
+            collection_path='/tenders/{tender_id}/awards/{award_id}/documents',
+            path='/tenders/{tender_id}/awards/{award_id}/documents/{document_id}',
+            procurementMethodType='aboveThresholdEU',
+            description="Tender award documents")
+class TenderAwardDocumentResource(BaseResource):
+    """ Tender Award Document """
