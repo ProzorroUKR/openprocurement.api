dc6906720518e2ac1e2a2070d25e232b60ac6c8a
Taras Kozlovskyi <ktarasz@quintagroup.com>
2016-02-01 13:57:07 +0200

    Updated check_status

---
+++ openprocurement/tender/openeu/views/award.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from logging import getLogger
+from openprocurement.api.utils import (
+    opresource,
+)
+
+from openprocurement.api.views.award import TenderAwardResource
+
+LOGGER = getLogger(__name__)
+
+
+@opresource(name='Tender UA Awards',
+            collection_path='/tenders/{tender_id}/awards',
+            path='/tenders/{tender_id}/awards/{award_id}',
+            description="Tender UA awards",
+            procurementMethodType='aboveThresholdEU')
+class TenderEUAwardResource(TenderAwardResource):
+    pass
