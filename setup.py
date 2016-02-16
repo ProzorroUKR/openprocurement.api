e0be5889c4d1a9b2e0987b19f2d095aba9413a7d
Myroslav Opyr <myroslav@quintagroup.com>
2016-02-16 20:44:09 +0200

    Fixed documentation buildout. Added Missing httpdomain dependency

--- setup.py
+++ setup.py
@@ -14,6 +14,10 @@ test_requires = requires + [
     'python-coveralls',
 ]
 
+docs_requires = requires + [
+    'sphinxcontrib-httpdomain',
+]
+
 entry_points = {
     'openprocurement.api.plugins': [
         'aboveThresholdEU = openprocurement.tender.openeu:includeme'
@@ -45,6 +49,6 @@ setup(name='openprocurement.tender.openeu',
       include_package_data=True,
       zip_safe=False,
       install_requires=requires,
-      extras_require={'test': test_requires},
+      extras_require={'test': test_requires, 'docs': docs_requires},
       test_suite="openprocurement.tender.openeu.tests.main.suite",
       entry_points=entry_points)
