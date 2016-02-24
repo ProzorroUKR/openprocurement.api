f1ee763c66305aa7ded8db28324b6b22a4f882a4
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2016-02-24 17:12:33 +0200

    register tender and include resources

--- openprocurement/tender/limited/__init__.py
+++ openprocurement/tender/limited/__init__.py
@@ -1,6 +1,11 @@
-from openprocurement.tender.limited.models import Tender
+from openprocurement.tender.limited.models import Tender, NegotiationTender
 
 
 def includeme(config):
     config.add_tender_procurementMethodType(Tender)
+    config.scan("openprocurement.tender.limited.views")
+
+
+def includeme_negotiation(config):
+    config.add_tender_procurementMethodType(NegotiationTender)
     config.scan("openprocurement.tender.limited.views")
