3096b27175077c08c7035da69010ce67ce89c9c1
Volodymyr Vozniak <volvoz@quintagroup.org>
2016-02-25 16:58:22 +0200

    test bids invalidation on tender change

--- openprocurement/tender/openeu/tests/bid.py
+++ openprocurement/tender/openeu/tests/bid.py
@@ -763,58 +763,90 @@ class TenderBidResourceTest(BaseTenderContentWebTest):
         self.assertEqual(response.status, '201 Created')
         valid_bid_id = response.json['data']['id']
 
-        # # change tender status
-        # self.set_status('active.qualification')
-        #
-        # # check tender status
-        # response = self.app.get('/tenders/{}'.format(self.tender_id))
-        # self.assertEqual(response.status, '200 OK')
-        # self.assertEqual(response.json['data']['status'], 'active.qualification')
-        # # tender should display all bids
-        # self.assertEqual(len(response.json['data']['bids']), 3)
-        # # invalidated bids should show only 'id' and 'status' fields
-        # for bid in response.json['data']['bids']:
-        #     if bid['status'] == 'invalid':
-        #         self.assertTrue('id' in bid)
-        #         self.assertFalse('value' in bid)
-        #         self.assertFalse('tenderers' in bid)
-        #         self.assertFalse('date' in bid)
-        #
-        # # invalidated bids stay invalidated
-        # for bid_id, token in bids_access.items():
-        #     response = self.app.get('/tenders/{}/bids/{}'.format(self.tender_id, bid_id))
-        #     self.assertEqual(response.status, '200 OK')
-        #     self.assertEqual(response.json['data']['status'], 'invalid')
-        #     # invalidated bids displays only 'id' and 'status' fields
-        #     self.assertFalse('value' in response.json['data'])
-        #     self.assertFalse('tenderers' in response.json['data'])
-        #     self.assertFalse('date' in response.json['data'])
-        #
-        # # and valid bid is not invalidated
-        # response = self.app.get('/tenders/{}/bids/{}'.format(self.tender_id, valid_bid_id))
-        # self.assertEqual(response.status, '200 OK')
-        # self.assertEqual(response.json['data']['status'], 'active')
-        # # and displays all his data
-        # self.assertTrue('value' in response.json['data'])
-        # self.assertTrue('tenderers' in response.json['data'])
-        # self.assertTrue('date' in response.json['data'])
-        #
-        # # check bids availability on finished tender
-        # self.set_status('complete')
-        # response = self.app.get('/tenders/{}'.format(self.tender_id))
-        # self.assertEqual(response.status, '200 OK')
-        # self.assertEqual(len(response.json['data']['bids']), 3)
-        # for bid in response.json['data']['bids']:
-        #     if bid['id'] in bids_access:  # previously invalidated bids
-        #         self.assertEqual(bid['status'], 'invalid')
-        #         self.assertFalse('value' in bid)
-        #         self.assertFalse('tenderers' in bid)
-        #         self.assertFalse('date' in bid)
-        #     else:  # valid bid
-        #         self.assertEqual(bid['status'], 'active')
-        #         self.assertTrue('value' in bid)
-        #         self.assertTrue('tenderers' in bid)
-        #         self.assertTrue('date' in bid)
+        response = self.app.post_json('/tenders/{}/bids'.format(
+            self.tender_id), {'data': {'tenderers': test_bids[1]['tenderers'], "value": {"amount": 101}}})
+
+        # switch to active.pre-qualification
+        self.set_status('active.pre-qualification', {"id": self.tender_id, 'status': 'active.tendering'})
+        self.app.authorization = ('Basic', ('chronograph', ''))
+        response = self.app.patch_json('/tenders/{}'.format(
+            self.tender_id), {"data": {"id": self.tender_id}})
+        self.assertEqual(response.json['data']['status'], 'active.pre-qualification')
+
+        # qualify bids
+        response = self.app.get('/tenders/{}/qualifications'.format(self.tender_id))
+        self.app.authorization = ('Basic', ('token', ''))
+        for qualification in response.json['data']:
+            response = self.app.patch_json('/tenders/{}/qualifications/{}'.format(
+            self.tender_id, qualification['id']), {"data": {"status": "active"}})
+            self.assertEqual(response.status, "200 OK")
+
+        # switch to active.pre-qualification.stand-still
+        response = self.app.patch_json('/tenders/{}'.format(
+            self.tender_id), {"data": {"status": 'active.pre-qualification.stand-still'}})
+        self.assertEqual(response.json['data']['status'], 'active.pre-qualification.stand-still')
+
+        # switch to active.auction
+        self.set_status('active.auction', {"id": self.tender_id, 'status': 'active.pre-qualification.stand-still'})
+        self.app.authorization = ('Basic', ('chronograph', ''))
+        response = self.app.patch_json('/tenders/{}'.format(
+            self.tender_id), {"data": {"id": self.tender_id}})
+        self.assertEqual(response.json['data']['status'], "active.auction")
+
+        # switch to qualification
+        self.app.authorization = ('Basic', ('auction', ''))
+        response = self.app.get('/tenders/{}/auction'.format(self.tender_id))
+        auction_bids_data = response.json['data']['bids']
+        response = self.app.post_json('/tenders/{}/auction'.format(self.tender_id),
+                                      {'data': {'bids': auction_bids_data}})
+        self.assertEqual(response.status, "200 OK")
+        response = self.app.get('/tenders/{}'.format(self.tender_id))
+        self.assertEqual(response.json['data']['status'], "active.qualification")
+        # tender should display all bids
+        self.assertEqual(len(response.json['data']['bids']), 4)
+        # invalidated bids should show only 'id' and 'status' fields
+        for bid in response.json['data']['bids']:
+            if bid['status'] == 'invalid':
+                self.assertTrue('id' in bid)
+                self.assertFalse('value' in bid)
+                self.assertFalse('tenderers' in bid)
+                self.assertFalse('date' in bid)
+
+        # invalidated bids stay invalidated
+        for bid_id, token in bids_access.items():
+            response = self.app.get('/tenders/{}/bids/{}'.format(self.tender_id, bid_id))
+            self.assertEqual(response.status, '200 OK')
+            self.assertEqual(response.json['data']['status'], 'invalid')
+            # invalidated bids displays only 'id' and 'status' fields
+            self.assertFalse('value' in response.json['data'])
+            self.assertFalse('tenderers' in response.json['data'])
+            self.assertFalse('date' in response.json['data'])
+
+        # and valid bid is not invalidated
+        response = self.app.get('/tenders/{}/bids/{}'.format(self.tender_id, valid_bid_id))
+        self.assertEqual(response.status, '200 OK')
+        self.assertEqual(response.json['data']['status'], 'active')
+        # and displays all his data
+        self.assertTrue('value' in response.json['data'])
+        self.assertTrue('tenderers' in response.json['data'])
+        self.assertTrue('date' in response.json['data'])
+
+        # check bids availability on finished tender
+        self.set_status('complete')
+        response = self.app.get('/tenders/{}'.format(self.tender_id))
+        self.assertEqual(response.status, '200 OK')
+        self.assertEqual(len(response.json['data']['bids']), 4)
+        for bid in response.json['data']['bids']:
+            if bid['id'] in bids_access:  # previously invalidated bids
+                self.assertEqual(bid['status'], 'invalid')
+                self.assertFalse('value' in bid)
+                self.assertFalse('tenderers' in bid)
+                self.assertFalse('date' in bid)
+            else:  # valid bid
+                self.assertEqual(bid['status'], 'active')
+                self.assertTrue('value' in bid)
+                self.assertTrue('tenderers' in bid)
+                self.assertTrue('date' in bid)
 
     def test_bids_activation_on_tender_documents(self):
         bids_access = {}
