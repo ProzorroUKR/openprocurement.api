25218e4b7cea3bd0615f1d37e54f4cd1cbc1ef4f
Roman Kozlovskyi <krzroman@gmail.com>
2016-03-16 13:51:39 +0200

    Updated list of required additional classifications

--- openprocurement/tender/limited/tests/tender.py
+++ openprocurement/tender/limited/tests/tender.py
@@ -418,7 +418,7 @@ class TenderResourceTest(BaseTenderWebTest):
         self.assertEqual(response.content_type, 'application/json')
         self.assertEqual(response.json['status'], 'error')
         self.assertEqual(response.json['errors'], [
-            {u'description': [{u'additionalClassifications': [u"One of additional classifications should be '\u0414\u041a\u041f\u041f'"]}], u'location': u'body', u'name': u'items'}
+            {u'description': [{u'additionalClassifications': [u"One of additional classifications should be one of [ДКПП, MEDT, ДК003, ДК015, ДК018]."]}], u'location': u'body', u'name': u'items'}
         ])
 
         data = self.initial_data["procuringEntity"]["contactPoint"]["telephone"]
