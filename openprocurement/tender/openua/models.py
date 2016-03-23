3d86813607c9e65b02c4814026f40a2beab4af2f
Taras Kozlovskyi <ktarasz@quintagroup.com>
2016-03-23 11:38:11 +0200

    Update validate_tenderPeriod

--- openprocurement/tender/openua/models.py
+++ openprocurement/tender/openua/models.py
@@ -444,7 +444,7 @@ class Tender(BaseTender):
 
     def validate_tenderPeriod(self, data, period):
         # if data['_rev'] is None when tender was created just now
-        if not data['_rev'] and calculate_business_date(get_now(), -timedelta(minutes=10), data) >= period.startDate:
+        if not data['_rev'] and calculate_business_date(get_now(), -timedelta(minutes=10)) >= period.startDate:
             raise ValidationError(u"tenderPeriod.startDate should be in greater than current date")
         if period and calculate_business_date(period.startDate, TENDER_PERIOD, data) > period.endDate:
             raise ValidationError(u"tenderPeriod should be greater than 15 days")
