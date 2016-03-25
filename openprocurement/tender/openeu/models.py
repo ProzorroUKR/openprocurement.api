8e0225496e345eb31946b98189aa9f4a01de2a28
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2016-03-25 17:01:05 +0200

    disable award.qualified and award.eligible validation

--- openprocurement/tender/openeu/models.py
+++ openprocurement/tender/openeu/models.py
@@ -342,6 +342,14 @@ class Award(BaseAward):
     complaints = ListType(ModelType(Complaint), default=list())
     items = ListType(ModelType(Item))
     documents = ListType(ModelType(Document), default=list())
+    qualified = BooleanType()
+    eligible = BooleanType()
+
+    def validate_qualified(self, data, qualified):
+        pass
+
+    def validate_eligible(self, data, eligible):
+        pass
 
 
 class Qualification(Model):
