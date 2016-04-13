0b101637b9f2d5b563fb588f9ce8e68e1ad07929
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2016-04-13 20:16:41 +0300

    setup.py definitions

--- setup.py
+++ setup.py
@@ -3,29 +3,43 @@ import os
 
 version = '1.0'
 
+requires = [
+    'setuptools',
+]
+
+test_requires = requires + [
+    'webtest',
+    'python-coveralls',
+]
+
+docs_requires = requires + [
+    'sphinxcontrib-httpdomain',
+]
+
 setup(name='openprocurement.contracting.api',
       version=version,
       description="",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
+        "Framework :: Pylons",
+        "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application"
         ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
-      license='GPL',
+      keywords="web services",
+      author='Quintagroup, Ltd.',
+      author_email='info@quintagroup.com',
+      license='Apache License 2.0',
+      url='https://github.com/openprocurement/openprocurement.api',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['openprocurement', 'openprocurement.contracting'],
       include_package_data=True,
       zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
+      install_requires=requires,
+      tests_require=test_requires,
+      extras_require={'test': test_requires, 'docs': docs_requires},
       entry_points="""
       # -*- Entry points: -*-
       """,
