2f2683fec7b72b3078d419eb8c619ea622d989dc
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2016-04-15 15:05:56 +0300

    define create, edit, view roles

--- openprocurement/contracting/api/models.py
+++ openprocurement/contracting/api/models.py
@@ -4,12 +4,32 @@ from couchdb_schematics.document import SchematicsDocument
 from schematics.types import StringType, BaseType
 from schematics.types.compound import ModelType, DictType
 from schematics.types.serializable import serializable
+from schematics.exceptions import ValidationError
+from schematics.transforms import whitelist
 from openprocurement.api.models import Contract as BaseContract
 from openprocurement.api.models import ListType, Revision, IsoDateTimeType
-from openprocurement.api.models import (
-    plain_role, create_role, edit_role, view_role, Administrator_role,
-    schematics_default_role)
+from openprocurement.api.models import (plain_role, Administrator_role,
+                                        schematics_default_role)
 from openprocurement.tender.openeu.models import Document as BaseDocument
+
+contract_create_role = (whitelist(
+    'id', 'awardID', 'contractID', 'contractNumber', 'title', 'title_en',
+    'title_ru', 'description', 'description_en', 'description_ru', 'status',
+    'period', 'value', 'dateSigned', 'documents', 'items', 'suppliers',
+    'owner', 'owner_token', 'tender_token', 'mode'
+))
+
+contract_edit_role = (whitelist(
+    'title', 'title_en', 'title_ru', 'description', 'description_en',
+    'description_ru', 'status', 'period', 'value' , 'mode'
+))
+
+contract_view_role = (whitelist(
+    'id', 'awardID', 'contractID', 'dateModified', 'contractNumber', 'title',
+    'title_en', 'title_ru', 'description', 'description_en', 'description_ru',
+    'status', 'period', 'value', 'dateSigned', 'documents', 'items',
+    'suppliers', 'owner', 'mode'
+))
 
 
 class IContract(Interface):
@@ -36,9 +56,9 @@ class Contract(SchematicsDocument, BaseContract):
     class Options:
         roles = {  # TODO
             'plain': plain_role,
-            'create': create_role,
-            'edit': edit_role,
-            'view': view_role,
+            'create': contract_create_role,
+            'edit': contract_edit_role,
+            'view': contract_view_role,
             'Administrator': Administrator_role,
             'default': schematics_default_role,
         }
