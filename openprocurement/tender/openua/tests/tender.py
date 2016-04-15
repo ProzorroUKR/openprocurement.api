c75f56bd6cea3aab5c568ec8ccf2fbcd9d82e47f
Roman Kozlovskyi <krzroman@gmail.com>
2016-04-15 13:27:03 +0300

    Disallow edit procuringEntity kind

--- openprocurement/tender/openua/tests/tender.py
+++ openprocurement/tender/openua/tests/tender.py
@@ -782,6 +782,11 @@ class TenderUAResourceTest(BaseTenderUAWebTest):
         self.assertEqual(response.content_type, 'application/json')
         self.assertNotEqual(response.json['data']['status'], 'cancelled')
 
+        response = self.app.patch_json('/tenders/{}?acc_token={}'.format(tender['id'], owner_token), {'data': {'procuringEntity': {'kind': 'defense'}}})
+        self.assertEqual(response.status, '200 OK')
+        self.assertEqual(response.content_type, 'application/json')
+        self.assertNotEqual(response.json['data']['procuringEntity']['kind'], 'defense')
+
         response = self.app.patch_json('/tenders/{}?acc_token={}'.format(tender['id'], owner_token),
             {'data': {'tenderPeriod': {'startDate': tender['enquiryPeriod']['endDate']}}},
             status=422
