d3d8b9e3875e9522e2097ae98ac41758154499cd
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2016-04-15 11:56:08 +0300

    contract creation tests

--- openprocurement/contracting/api/tests/contract.py
+++ openprocurement/contracting/api/tests/contract.py
@@ -313,6 +313,115 @@ class ContractResourceTest(BaseWebTest):
             {u'description': u'Not Found', u'location': u'url', u'name': u'contract_id'}
         ])
 
+    def test_create_contract_invalid(self):
+        request_path = '/contracts'
+        response = self.app.post(request_path, 'data', status=415)
+        self.assertEqual(response.status, '415 Unsupported Media Type')
+        self.assertEqual(response.content_type, 'application/json')
+        self.assertEqual(response.json['status'], 'error')
+        self.assertEqual(response.json['errors'], [
+            {u'description':
+                 u"Content-Type header should be one of ['application/json']", u'location': u'header',
+             u'name': u'Content-Type'}
+        ])
+
+        response = self.app.post(
+            request_path, 'data', content_type='application/json', status=422)
+        self.assertEqual(response.status, '422 Unprocessable Entity')
+        self.assertEqual(response.content_type, 'application/json')
+        self.assertEqual(response.json['status'], 'error')
+        self.assertEqual(response.json['errors'], [
+            {u'description': u'No JSON object could be decoded',
+             u'location': u'body', u'name': u'data'}
+        ])
+
+        response = self.app.post_json(request_path, 'data', status=422)
+        self.assertEqual(response.status, '422 Unprocessable Entity')
+        self.assertEqual(response.content_type, 'application/json')
+        self.assertEqual(response.json['status'], 'error')
+        self.assertEqual(response.json['errors'], [
+            {u'description': u'Data not available',
+             u'location': u'body', u'name': u'data'}
+        ])
+
+        response = self.app.post_json(request_path, {'not_data': {}}, status=422)
+        self.assertEqual(response.status, '422 Unprocessable Entity')
+        self.assertEqual(response.content_type, 'application/json')
+        self.assertEqual(response.json['status'], 'error')
+        self.assertEqual(response.json['errors'], [
+            {u'description': u'Data not available',
+             u'location': u'body', u'name': u'data'}
+        ])
+
+        response = self.app.post_json(request_path, {'data': []}, status=422)
+        self.assertEqual(response.status, '422 Unprocessable Entity')
+        self.assertEqual(response.content_type, 'application/json')
+        self.assertEqual(response.json['status'], 'error')
+        self.assertEqual(response.json['errors'], [
+            {u'description': u'Data not available',
+             u'location': u'body', u'name': u'data'}
+        ])
+
+        response = self.app.post_json(request_path, {'data': {
+            'invalid_field': 'invalid_value'}}, status=422)
+        self.assertEqual(response.status, '422 Unprocessable Entity')
+        self.assertEqual(response.content_type, 'application/json')
+        self.assertEqual(response.json['status'], 'error')
+        self.assertEqual(response.json['errors'], [
+            {u'description': u'Rogue field', u'location':
+                u'body', u'name': u'invalid_field'}
+        ])
+
+    def test_create_contract_generated(self):
+        data = test_contract_data.copy()
+        data.update({'id': uuid4().hex, 'doc_id': uuid4().hex, 'contractID': uuid4().hex})
+        response = self.app.post_json('/contracts', {'data': data})
+        self.assertEqual(response.status, '201 Created')
+        self.assertEqual(response.content_type, 'application/json')
+        contract = response.json['data']
+        self.assertEqual(set(contract), set([
+            u'id', u'dateModified', u'contractID', u'status', u'suppliers',
+            u'contractNumber', u'period', u'dateSigned', u'value', u'awardID',
+            u'items']))
+        self.assertEqual(data['id'], contract['id'])
+        self.assertNotEqual(data['doc_id'], contract['id'])
+        self.assertEqual(data['contractID'], contract['contractID'])
+
+    def test_create_contract(self):
+        response = self.app.get('/contracts')
+        self.assertEqual(response.status, '200 OK')
+        self.assertEqual(len(response.json['data']), 0)
+
+        response = self.app.post_json('/contracts', {"data": test_contract_data})
+        self.assertEqual(response.status, '201 Created')
+        self.assertEqual(response.content_type, 'application/json')
+        contract = response.json['data']
+
+        response = self.app.get('/contracts/{}'.format(contract['id']))
+        self.assertEqual(response.status, '200 OK')
+        self.assertEqual(response.content_type, 'application/json')
+        self.assertEqual(set(response.json['data']), set(contract))
+        self.assertEqual(response.json['data'], contract)
+
+        data = deepcopy(test_contract_data)
+        data['id'] = uuid4().hex
+        response = self.app.post_json('/contracts?opt_jsonp=callback', {"data": data})
+        self.assertEqual(response.status, '201 Created')
+        self.assertEqual(response.content_type, 'application/javascript')
+        self.assertIn('callback({"', response.body)
+
+        data['id'] = uuid4().hex
+        response = self.app.post_json('/contracts?opt_pretty=1', {"data": data})
+        self.assertEqual(response.status, '201 Created')
+        self.assertEqual(response.content_type, 'application/json')
+        self.assertIn('{\n    "', response.body)
+
+        data['id'] = uuid4().hex
+        response = self.app.post_json('/contracts', {"data": data, "options": {"pretty": True}})
+        self.assertEqual(response.status, '201 Created')
+        self.assertEqual(response.content_type, 'application/json')
+        self.assertIn('{\n    "', response.body)
+
 
 def suite():
     suite = unittest.TestSuite()
