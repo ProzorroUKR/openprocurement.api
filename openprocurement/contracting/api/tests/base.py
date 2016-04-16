c14dd83911b984c35e7292c2ee80274fe3813f77
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2016-04-16 16:35:52 +0300

    construct tecting contract with access attributes set

--- openprocurement/contracting/api/tests/base.py
+++ openprocurement/contracting/api/tests/base.py
@@ -148,11 +148,20 @@ class BaseContractWebTest(BaseWebTest):
         orig_auth = self.app.authorization
         self.app.authorization = ('Basic', ('databridge', ''))
         response = self.app.post_json('/contracts', {'data': data})
-        contract = response.json['data']
-        self.contract_token = response.json['access']['token']
-        self.contract_id = contract['id']
+        self.contract = response.json['data']
+        # self.contract_token = response.json['access']['token']
+        self.contract_id = self.contract['id']
         self.app.authorization = orig_auth
 
     def tearDown(self):
         del self.db[self.contract_id]
         super(BaseContractWebTest, self).tearDown()
+
+
+class BaseContractContentWebTest(BaseContractWebTest):
+
+    def setUp(self):
+        super(BaseContractContentWebTest, self).setUp()
+        response = self.app.patch_json('/contracts/{}/credentials?acc_token={}'.format(
+            self.contract_id, self.initial_data['tender_token']), {'data': {}})
+        self.contract_token = response.json['access']['token']
