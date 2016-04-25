d7d79ab8fa9dde9d8dc73cbbec5ecac4e6a54446
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2016-04-25 18:17:02 +0300

    register /tenders/{tender_id}/extract_credentials end-point

---
+++ openprocurement/contracting/api/views/tenders.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from hashlib import md5
+from openprocurement.api.utils import (
+    json_view,
+    opresource,
+    APIResource,
+)
+
+
+@opresource(name='Tender credentials',
+            path='/tenders/{tender_id}/extract_credentials',
+            description="Open Contracting compatible data exchange format. See http://ocds.open-contracting.org/standard/r/master/#tender for more info")
+class TenderResource(APIResource):
+
+    @json_view(permission='extract_credentials')
+    def get(self):
+        self.LOGGER.info('Extract credentials for tender {}'.format(self.context.id))
+        tender = self.request.validated['tender']
+        data = tender.serialize('contracting')
+        data['tender_token'] = md5(tender.owner_token).hexdigest()
+        return {'data': data}
