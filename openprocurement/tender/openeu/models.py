15f629becdda687ff1a0cc95431469fedc059f19
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2016-04-28 16:25:08 +0300

    added 'contracting' role

--- openprocurement/tender/openeu/models.py
+++ openprocurement/tender/openeu/models.py
@@ -436,6 +436,7 @@ class Tender(BaseTender):
             'chronograph_view': chronograph_view_role,
             'Administrator': Administrator_role,
             'default': schematics_default_role,
+            'contracting': whitelist('doc_id', 'owner'),
         }
 
     procurementMethodType = StringType(default="aboveThresholdEU")
