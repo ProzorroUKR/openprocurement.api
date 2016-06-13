b574624537cd51cc17df431a27475f3484ba3344
oleh <chezar1995@gmail.com>
2016-06-13 15:55:28 +0300

    pep8 utils.py change superclass for CompetitiveDialogueEUComplaintResource and CompetitiveDialogueUAComplaintResource

--- openprocurement/tender/competitivedialogue/views/complaint.py
+++ openprocurement/tender/competitivedialogue/views/complaint.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from openprocurement.api.utils import opresource
-from openprocurement.tender.openua.views.complaint import TenderUaComplaintResource
+from openprocurement.tender.openeu.views.complaint import TenderEUComplaintResource
 
 
 @opresource(name='Competitive Dialogue EU Complaints',
@@ -8,10 +8,8 @@ from openprocurement.tender.openua.views.complaint import TenderUaComplaintResou
             path='/tenders/{tender_id}/complaints/{complaint_id}',
             procurementMethodType='competitiveDialogue.aboveThresholdEU',
             description="Competitive Dialogue EU complaints")
-class CompetitiveDialogueEUComplaintResource(TenderUaComplaintResource):
-
-    def complaints_len(self, tender):
-        return sum([len(i.complaints) for i in tender.awards], sum([len(i.complaints) for i in tender.qualifications], len(tender.complaints)))
+class CompetitiveDialogueEUComplaintResource(TenderEUComplaintResource):
+    pass
 
 
 @opresource(name='Competitive Dialogue UA Complaints',
@@ -19,7 +17,6 @@ class CompetitiveDialogueEUComplaintResource(TenderUaComplaintResource):
             path='/tenders/{tender_id}/complaints/{complaint_id}',
             procurementMethodType='competitiveDialogue.aboveThresholdUA',
             description="Competitive Dialogue UA complaints")
-class CompetitiveDialogueUAComplaintResource(TenderUaComplaintResource):
-    def complaints_len(self, tender):
-        return sum([len(i.complaints) for i in tender.awards],
-                   sum([len(i.complaints) for i in tender.qualifications], len(tender.complaints)))
+class CompetitiveDialogueUAComplaintResource(TenderEUComplaintResource):
+    pass
+
