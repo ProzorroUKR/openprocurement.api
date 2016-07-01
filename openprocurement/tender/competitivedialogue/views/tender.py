9c90d4bcbd89eb6844487d75c0c3987ff5989738
oleh <chezar1995@gmail.com>
2016-07-01 12:30:19 +0300

    remove copy-paste  code TenderStage2EUCredentialsResource inherited from APIResource  TenderStage2UACredentialsResource inherited from TenderStage2EUCredentialsResource

--- openprocurement/tender/competitivedialogue/views/tender.py
+++ openprocurement/tender/competitivedialogue/views/tender.py
@@ -3,7 +3,7 @@ from openprocurement.api.views.tender import TenderResource
 from openprocurement.tender.openeu.views.tender import TenderEUResource
 from openprocurement.tender.openua.validation import validate_patch_tender_ua_data
 from openprocurement.tender.competitivedialogue.utils import patch_eu, set_ownership
-from openprocurement.api.utils import opresource, json_view, save_tender, context_unpack
+from openprocurement.api.utils import opresource, json_view, save_tender, context_unpack, APIResource
 from openprocurement.tender.competitivedialogue.models import CD_EU_TYPE, CD_UA_TYPE, STAGE_2_EU_TYPE, STAGE_2_UA_TYPE
 
 
@@ -59,10 +59,7 @@ class TenderStage2UEResource(TenderResource):
             path='/tenders/{tender_id}/credentials',
             procurementMethodType=STAGE_2_EU_TYPE,
             description="Tender stage2 UE credentials")
-class TenderStage2EUCredentialsResource(TenderResource):
-    def __init__(self, request, context):
-        super(TenderStage2EUCredentialsResource, self).__init__(request, context)
-        self.server = request.registry.couchdb_server
+class TenderStage2EUCredentialsResource(APIResource):
 
     @json_view(permission='generate_credentials')
     def patch(self):
@@ -90,28 +87,5 @@ class TenderStage2EUCredentialsResource(TenderResource):
             path='/tenders/{tender_id}/credentials',
             procurementMethodType=STAGE_2_UA_TYPE,
             description="Tender stage2 UA credentials")
-class TenderStage2UACredentialsResource(TenderResource):
-    def __init__(self, request, context):
-        super(TenderStage2UACredentialsResource, self).__init__(request, context)
-        self.server = request.registry.couchdb_server
-
-    @json_view(permission='generate_credentials')
-    def patch(self):
-        tender = self.request.validated['tender']
-        if tender.status != "draft":
-            self.request.errors.add('body', 'data',
-                                    'Can\'t generate credentials in current ({}) contract status'.format(
-                                        tender.status))
-            self.request.errors.status = 403
-            return
-
-        set_ownership(tender)
-        if save_tender(self.request):
-            self.LOGGER.info('Generate Tender stage2 credentials {}'.format(tender.id),
-                             extra=context_unpack(self.request, {'MESSAGE_ID': 'tender_patch'}))
-            return {
-                'data': tender.serialize("view"),
-                'access': {
-                    'token': tender.owner_token
-                }
-            }
+class TenderStage2UACredentialsResource(TenderStage2EUCredentialsResource):
+    pass
