f622005d67752aa032b8317b99d262189fde0177
Roman Kozlovskyi <krzroman@gmail.com>
2016-07-01 16:15:46 +0300

    Added migration urls

---
+++ openprocurement/planning/api/migration.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+import logging
+from datetime import timedelta
+from iso8601 import parse_date
+from openprocurement.api.models import CPV_CODES, STAND_STILL_TIME, TZ, get_now
+from openprocurement.planning.api.traversal import Root
+from openprocurement.planning.api.models import Plan
+from email.header import decode_header
+
+
+LOGGER = logging.getLogger(__name__)
+SCHEMA_VERSION = 1
+SCHEMA_DOC = 'openprocurement_plans_schema'
+
+
+def get_db_schema_version(db):
+    schema_doc = db.get(SCHEMA_DOC, {"_id": SCHEMA_DOC})
+    return schema_doc.get("version", SCHEMA_VERSION - 1)
+
+
+def set_db_schema_version(db, version):
+    schema_doc = db.get(SCHEMA_DOC, {"_id": SCHEMA_DOC})
+    schema_doc["version"] = version
+    db.save(schema_doc)
+
+
+def migrate_data(registry, destination=None):
+    cur_version = get_db_schema_version(registry.db)
+    if cur_version == SCHEMA_VERSION:
+        return cur_version
+    for step in xrange(cur_version, destination or SCHEMA_VERSION):
+        LOGGER.info("Migrate openprocurement schema from {} to {}".format(step, step + 1), extra={'MESSAGE_ID': 'migrate_data'})
+        migration_func = globals().get('from{}to{}'.format(step, step + 1))
+        if migration_func:
+            migration_func(registry)
+        set_db_schema_version(registry.db, step + 1)
+
+
+def from0to1(registry):
+    class Request(object):
+        def __init__(self, registry):
+            self.registry = registry
+    results = registry.db.iterview('plans/all', 2 ** 10, include_docs=True)
+    docs = []
+    request = Request(registry)
+    root = Root(request)
+    for i in results:
+        doc = i.doc
+        if doc.get('documents'):
+            plan = Plan(doc)
+            plan.__parent__ = root
+            doc = plan.to_primitive()
+            docs.append(doc)
+        if len(docs) >= 2 ** 7:
+            result = registry.db.update(docs)
+            docs = []
+    if docs:
+        registry.db.update(docs)
