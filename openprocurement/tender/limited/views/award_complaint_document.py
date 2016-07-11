bacc46537c4845abd257f4a3f0df00010e4ec705
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2016-07-11 20:35:20 +0300

    use self-defined statuses

--- openprocurement/tender/limited/views/award_complaint_document.py
+++ openprocurement/tender/limited/views/award_complaint_document.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 from openprocurement.api.utils import opresource
 from openprocurement.tender.openua.views.award_complaint_document import TenderUaAwardComplaintDocumentResource
-from openprocurement.tender.openua.views.complaint_document import STATUS4ROLE
+
+STATUS4ROLE = {
+    'complaint_owner': ['draft', 'answered', 'claim'],
+    'aboveThresholdReviewers': ['pending', 'accepted', 'stopping'],
+    'tender_owner': ['claim', 'satisfied'],
+}
 
 
 @opresource(name='Tender negotiation Award Complaint Documents',
