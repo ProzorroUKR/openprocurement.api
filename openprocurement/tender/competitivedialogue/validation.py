a2058909315c06afa1fecbb8d8701f245715d44a
Krokop <chezar1995@gmail.com>
2016-08-02 11:38:31 +0300

    add validation for post on question add tests

--- openprocurement/tender/competitivedialogue/validation.py
+++ openprocurement/tender/competitivedialogue/validation.py
@@ -42,14 +42,14 @@ def validate_patch_tender_stage2_data(request):
     return data
 
 
-def validate_author(request, shortlistedFirms, complaint):
+def validate_author(request, shortlistedFirms, obj, error_message='Author can\'t create complaint'):
     firms_key = prepare_shortlistedFirms(shortlistedFirms)
-    author_key = prepare_author(complaint)
+    author_key = prepare_author(obj)
     for firm in firms_key:
         if author_key in firm:  # if we found legal firm then check another complaint
             break
     else:  # we didn't find legal firm, then return error
-        request.errors.add('body', 'author', 'Author can\'t create complaint')
+        request.errors.add('body', 'author', error_message)
         request.errors.status = 403
         return False
     return True
@@ -79,8 +79,33 @@ def validate_patch_complaint_data_stage2(request):
     model = type(request.tender).complaints.model_class
     data = validate_data(request, model, True)
     if data:
-        if validate_author(request, request.tender['shortlistedFirms'], request.validated['complaint']):
+        if validate_author(request, request.tender['shortlistedFirms'],
+                           request.validated['complaint'],
+                           'Author can\'t patch complaint'):
             return data  # validate is OK
         else:
             return None  # we catch errors
     return data
+
+
+def validate_post_question_data_stage2(request):
+    if not request.check_accreditation(request.tender.edit_accreditation):
+        request.errors.add('procurementMethodType', 'accreditation', 'Broker Accreditation level does not permit question creation')
+        request.errors.status = 403
+        return
+    if request.tender.get('mode', None) is None and request.check_accreditation('t'):
+        request.errors.add('procurementMethodType', 'mode', 'Broker Accreditation level does not permit question creation')
+        request.errors.status = 403
+        return
+    update_logging_context(request, {'question_id': '__new__'})
+    model = type(request.tender).questions.model_class
+    data = validate_data(request, model)
+    if data:
+        if validate_author(request, request.tender['shortlistedFirms'],
+                           request.validated['question'],
+                           'Author can\'t create question'):
+            return data  # validate is OK
+        else:
+            return None  # we catch errors
+    return data
+
