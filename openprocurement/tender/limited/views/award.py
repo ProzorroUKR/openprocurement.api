1f7dc91a5ef3c1833758a1837664079d5f793ab8
Roman Kozlovskyi <krzroman@gmail.com>
2016-08-08 14:04:27 +0300

    Switch off award qualified validation

--- openprocurement/tender/limited/views/award.py
+++ openprocurement/tender/limited/views/award.py
@@ -430,10 +430,10 @@ class TenderNegotiationAwardResource(TenderAwardResource):
             self.request.errors.status = 403
             return
         award = self.request.validated['award']
-        if award.status == "active" and not award.qualified:
-            self.request.errors.add('body', 'data', 'Can\'t create new award in active status and not qualified')
-            self.request.errors.status = 403
-            return
+        #if award.status == "active" and not award.qualified:
+            #self.request.errors.add('body', 'data', 'Can\'t create new award in active status and not qualified')
+            #self.request.errors.status = 403
+            #return
         award.complaintPeriod = {'startDate': get_now().isoformat()}
         tender.awards.append(award)
         if save_tender(self.request):
@@ -510,10 +510,10 @@ class TenderNegotiationAwardResource(TenderAwardResource):
         award = self.request.context
         award_status = award.status
         apply_patch(self.request, save=False, src=self.request.context.serialize())
-        if award.status == "active" and not award.qualified:
-            self.request.errors.add('body', 'data', 'Can\'t update award to active status with not qualified')
-            self.request.errors.status = 403
-            return
+        #if award.status == "active" and not award.qualified:
+            #self.request.errors.add('body', 'data', 'Can\'t update award to active status with not qualified')
+            #self.request.errors.status = 403
+            #return
         if award_status == 'pending' and award.status == 'active':
             normalized_end = calculate_normalized_date(get_now(), tender, True)
             award.complaintPeriod.endDate = calculate_business_date(normalized_end, self.stand_still_delta, tender)
