1af44793266e8fd28c1a23cbf44ad5fbb743353b
Oleksandr Vinnichuk <oleksandr.v@quintagroup.org>
2016-11-07 19:36:39 +0200

    Add raise in backword and forward exceptions

--- openprocurement/contracting/api/databridge.py
+++ openprocurement/contracting/api/databridge.py
@@ -403,6 +403,7 @@ class ContractingDataBridge(object):
             # TODO reset queues and restart sync
             logger.warn('Forward worker died!', extra=journal_context({"MESSAGE_ID": DATABRIDGE_WORKER_DIED}, {}))
             logger.exception(e)
+            raise
         else:
             logger.warn('Forward data sync finished!', extra=journal_context({"MESSAGE_ID": DATABRIDGE_WORKER_DIED}, {}))  # Should never happen!!!
 
@@ -423,6 +424,7 @@ class ContractingDataBridge(object):
             # TODO reset queues and restart sync
             logger.warn('Backward worker died!', extra=journal_context({"MESSAGE_ID": DATABRIDGE_WORKER_DIED}, {}))
             logger.exception(e)
+            raise
         else:
             logger.info('Backward data sync finished.')
 
