e6acced28284e16f791af23a9d8e41d352ac7120
Yaroslav Shalenyk <yshalenyk@quinatagroup.org>
2016-12-27 13:32:15 +0200

    Add new tests

--- openprocurement/historical/tender/tests/tests.py
+++ openprocurement/historical/tender/tests/tests.py
@@ -1,25 +1,45 @@
 import os.path
+import logging
+from jsonpointer import resolve_pointer
 from openprocurement.historical.core.tests import test_data_with_revisions
-from openprocurement.api.tests.base import BaseTenderWebTest as BaseBaseTenderWebTest, test_tender_data, BaseTenderWebTest
+from openprocurement.historical.core.utils import HEADER
+from openprocurement.api.tests.base import BaseTenderWebTest, BaseWebTest
 
 
-class BaseTenderHistoryWebTest(BaseBaseTenderWebTest):
+test_data = test_data_with_revisions.copy()
+revisions = test_data.get('revisions')
+public_revisions = [r for r in revisions if r['public']]
+
+
+class BaseTenderHistoryWebTest(BaseTenderWebTest):
     relative_to = os.path.dirname(__file__)
+    initial_data = test_data
+    setUp = BaseWebTest.setUp
 
 
 class HistoricalTenderTestCase(BaseTenderHistoryWebTest):
-    setUp = BaseTenderWebTest.setUp
-    #initial_data = test_data_with_revisions
+
     def setUp(self):
         super(HistoricalTenderTestCase, self).setUp()
-        test_data_with_revisions['doc_type'] = 'Tender'
-        test_data_with_revisions['_id'] = test_data_with_revisions['id']
-        self.doc_id, _ = self.db.save(test_data_with_revisions)
+        self.tender_id, _ = self.db.save(test_data)
 
     def test_get_tender(self):
-        #self.app.post_json(self
-        #response = self.app.post_json('/tenders', {'data': test_tender_data})
-        response = self.app.get('/tenders/{}/historical'.format(self.doc_id))
-        #response = self.app.get('/tenders/{}/historical'.format(self.doc_id))
+        response = self.app.get('/tenders/{}/historical'.format(self.tender_id))
         self.assertEqual(response.status, '200 OK')
-        #self.assertEqual(len(response.json['data']), 1)
+        self.assertIn(HEADER, response.headers)
+        self.assertEqual(response.headers[HEADER], str(len(public_revisions)))
+
+    def test_get_tender_versions(self):
+        for ver, rev in list(enumerate(public_revisions))[1:]:
+            response = self.app.get('/tenders/{}/historical'.format(self.tender_id),
+                                    headers={HEADER: str(ver)})
+            self.assertEqual(response.headers[HEADER], str(ver))
+
+            body = response.json['data']
+            self.assertEqual(body['dateModified'],
+                             public_revisions[int(ver) - 1]['date'])
+            self.assertEqual(public_revisions[int(response.headers[HEADER])], rev)
+            for p in rev['changes']:
+                if not any(v in p['path'] for v in ['next_check', 'shouldStartAfter']):
+                    val = p['value'] if p['op'] != 'remove' else 'missing'
+                    self.assertEqual(resolve_pointer(body, p['path'], 'missing'), val)
