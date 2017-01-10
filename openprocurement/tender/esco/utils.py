f10ffc330261939785843d2197fb23ef7ba336d3
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2017-01-10 19:39:21 +0200

    add npv calculation

---
+++ openprocurement/tender/esco/utils.py
@@ -0,0 +1,10 @@
+from openprocurement.tender.esco.constants import NPV_CALCULATION_DURATION
+
+
+def calculate_npv(nbu_rate, annualCostsReduction, yearlyPayments,
+                  contractDuration):
+    CRfree = annualCostsReduction
+    CR = CRfree - annualCostsReduction * yearlyPayments
+    value = sum([(CR and i <= contractDuration or CRfree)/(1 + nbu_rate)**i
+                 for i in range(1, NPV_CALCULATION_DURATION + 1)])
+    return value
