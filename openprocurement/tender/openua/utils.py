c24240ae67c20ecc7786e59d14bb07d14af7ebca
Roman Kozlovskyi <krzroman@gmail.com>
2017-01-12 16:00:33 +0200

    Fixed check unanswered questions

--- openprocurement/tender/openua/utils.py
+++ openprocurement/tender/openua/utils.py
@@ -37,8 +37,13 @@ def check_complaint_status(request, complaint):
 def has_unanswered_questions(tender, filter_cancelled_lots=True):
     if filter_cancelled_lots and tender.lots:
         active_lots = [l.id for l in tender.lots if l.status == 'active']
-        return any([i.id for i in tender.questions if i.relatedItem in active_lots and not i.answer])
-    return any([i.id for i in tender.questions if not i.answer])
+        active_items = [i.id for i in tender.items if not i.relatedLot or i.relatedLot in active_lots]
+        return any([
+            not i.answer
+            for i in tender.questions
+            if i.questionOf == 'tender' or i.questionOf == 'lot' and i.relatedItem in active_lots or i.questionOf == 'item' and i.relatedItem in active_items
+        ])
+    return any([not i.answer for i in tender.questions])
 
 
 def has_unanswered_complaints(tender, filter_cancelled_lots=True):
