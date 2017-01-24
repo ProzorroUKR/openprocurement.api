86cc21e4089d0f157784f265e5a88ec471ad1a8a
Yaroslav Shalenyk <yshalenyk@quinatagroup.org>
2017-01-24 16:39:04 +0200

    Call external view from api regisry

--- openprocurement/historical/tender/views/tender.py
+++ openprocurement/historical/tender/views/tender.py
@@ -1,13 +1,27 @@
-from openprocurement.historical.tender.utils import tenders_history_resource
-from openprocurement.api.utils import json_view, APIResource
+from openprocurement.historical.tender.utils import (
+    tenders_history_resource
+)
+
+from openprocurement.historical.core.utils import (
+    get_route,
+    call_view,
+    return404
+)
+
+from openprocurement.api.utils import (
+    json_view,
+    APIResource
+)
 
 
 @tenders_history_resource(name='TenderHistory',
-                          path='/tenders/{tender_id}/historical',
+                          path='/tenders/{doc_id}/historical',
                           description="Open Contracting compatible data exchange format. See http://ocds.open-contracting.org/standard/r/master/#tender for more info")
 class TenderHistoryResource(APIResource):
 
     @json_view(permission="view_tender")
     def get(self):
-        tender = self.request.validated['tender']
-        return {'data': tender.serialize(tender.status)}
+        route = get_route(self.request)
+        if route is None:
+            return404(self.request, 'url', 'tender_id')
+        return call_view(self.request, self.context, route)
