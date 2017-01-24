c3f1894a060f3d926804b58223add21f90b4813f
Roman Kozlovskyi <krzroman@gmail.com>
2017-01-24 13:00:44 +0200

    Allow specialNorms additionalClassifications

--- openprocurement/tender/openeu/tests/tender.py
+++ openprocurement/tender/openeu/tests/tender.py
@@ -539,7 +539,7 @@ class TenderResourceTest(BaseTenderWebTest):
         self.assertEqual(response.json['status'], 'error')
         if get_now() > CPV_ITEMS_CLASS_FROM:
             self.assertEqual(response.json['errors'], [
-                {u'description': [{u'additionalClassifications': [u"One of additional classifications should be one of [ДК003, ДК015, ДК018]."]}], u'location': u'body', u'name': u'items'}
+                {u'description': [{u'additionalClassifications': [u"One of additional classifications should be one of [ДК003, ДК015, ДК018, specialNorms]."]}], u'location': u'body', u'name': u'items'}
             ])
         else:
             self.assertEqual(response.json['errors'], [
