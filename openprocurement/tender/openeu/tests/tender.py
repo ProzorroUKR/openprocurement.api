318901c7a17a8120d5dc72ec363543c151452eb3
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2017-01-26 13:13:30 +0200

    deliveryAddress fields are not required

--- openprocurement/tender/openeu/tests/tender.py
+++ openprocurement/tender/openeu/tests/tender.py
@@ -573,15 +573,13 @@ class TenderResourceTest(BaseTenderWebTest):
         ])
 
         data = deepcopy(test_tender_data)
-        del data["items"][0]['deliveryAddress']['postalCode']
-        del data["items"][0]['deliveryAddress']['locality']
         del data["items"][0]['deliveryDate']
         response = self.app.post_json(request_path, {'data': data}, status=422)
         self.assertEqual(response.status, '422 Unprocessable Entity')
         self.assertEqual(response.content_type, 'application/json')
         self.assertEqual(response.json['status'], 'error')
         self.assertEqual(response.json['errors'], [
-            {u'description': [{u'deliveryDate': [u'This field is required.'], u'deliveryAddress': {u'postalCode': [u'This field is required.'], u'locality': [u'This field is required.']}}], u'location': u'body', u'name': u'items'}
+            {u'description': [{u'deliveryDate': [u'This field is required.']}], u'location': u'body', u'name': u'items'}
         ])
 
     def test_create_tender_generated(self):
@@ -683,6 +681,19 @@ class TenderResourceTest(BaseTenderWebTest):
         self.assertIn('guarantee', data)
         self.assertEqual(data['guarantee']['amount'], 100500)
         self.assertEqual(data['guarantee']['currency'], "USD")
+
+        data = deepcopy(test_tender_data)
+        del data["items"][0]['deliveryAddress']['postalCode']
+        del data["items"][0]['deliveryAddress']['locality']
+        del data["items"][0]['deliveryAddress']['streetAddress']
+        del data["items"][0]['deliveryAddress']['region']
+        response = self.app.post_json('/tenders', {'data': data})
+        self.assertEqual(response.status, '201 Created')
+        self.assertEqual(response.content_type, 'application/json')
+        self.assertNotIn('postalCode', response.json['data']['items'][0]['deliveryAddress'])
+        self.assertNotIn('locality', response.json['data']['items'][0]['deliveryAddress'])
+        self.assertNotIn('streetAddress', response.json['data']['items'][0]['deliveryAddress'])
+        self.assertNotIn('region', response.json['data']['items'][0]['deliveryAddress'])
 
     def test_get_tender(self):
         response = self.app.get('/tenders')
