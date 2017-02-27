416d68c33c5ddeb67e5ea2edb4b102e6fe5fce86
Yaroslav Neznaradko <yarsanich@quintagroup.org>
2017-02-27 17:58:36 +0200

    expression without else

--- openprocurement/tender/openua/__init__.py
+++ openprocurement/tender/openua/__init__.py
@@ -4,8 +4,6 @@ from openprocurement.tender.core import includeme as includeme_core
 def includeme(config):
     if not hasattr(config, 'add_tender_procurementMethodType'):
         includeme_core(config)
-        config.add_tender_procurementMethodType(Tender)
-    else:
-        config.add_tender_procurementMethodType(Tender)
+    config.add_tender_procurementMethodType(Tender)
     print "init openua"
     config.scan("openprocurement.tender.openua.views")
