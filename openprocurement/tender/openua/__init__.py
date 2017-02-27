fcf34aeb81cb68dcd71638d5c3d59ec3b0ac334b
Yaroslav Neznaradko <yarsanich@quintagroup.org>
2017-02-27 17:16:31 +0200

    include tender_core first

--- openprocurement/tender/openua/__init__.py
+++ openprocurement/tender/openua/__init__.py
@@ -1,6 +1,11 @@
 from openprocurement.tender.openua.models import Tender
-
+from openprocurement.tender.core import includeme as includeme_core
 
 def includeme(config):
-    config.add_tender_procurementMethodType(Tender)
+    if not hasattr(config, 'add_tender_procurementMethodType'):
+        includeme_core(config)
+        config.add_tender_procurementMethodType(Tender)
+    else:
+        config.add_tender_procurementMethodType(Tender)
+    print "init openua"
     config.scan("openprocurement.tender.openua.views")
