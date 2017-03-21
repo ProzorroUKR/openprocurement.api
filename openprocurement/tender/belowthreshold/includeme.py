c672d15f251c20704335aea00ad33e19f4ed07a4
Yaroslav Neznaradko <yarsanich@quintagroup.org>
2017-03-21 15:52:40 +0200

    remove print info

--- openprocurement/tender/belowthreshold/includeme.py
+++ openprocurement/tender/belowthreshold/includeme.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from openprocurement.tender.belowthreshold.models import Tender
 
+
 def includeme(config):
     config.add_tender_procurementMethodType(Tender)
-    print "Init below"
     config.scan("openprocurement.tender.belowthreshold.views")
