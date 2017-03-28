a27fd931e09f142911af5a9c53edb7948c67b297
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2017-03-28 12:30:54 +0300

    update interface name

--- openprocurement/tender/belowthreshold/includeme.py
+++ openprocurement/tender/belowthreshold/includeme.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from pyramid.interfaces import IRequest
-from openprocurement.api.interfaces import IContextConfigurator
+from openprocurement.api.interfaces import IContentConfigurator
 from openprocurement.tender.belowthreshold.models import Tender, IBelowThresholdTender
 from openprocurement.tender.belowthreshold.adapters import TenderBelowThersholdConfigurator
 
@@ -11,4 +11,4 @@ def includeme(config):
     config.scan("openprocurement.tender.belowthreshold.subscribers")
     # config.registry.registerAdapter(TenderBelowThersholdConfigurator,
                                     # (IBelowThresholdTender, IRequest),
-                                    # IContextConfigurator)
+                                    # IContentConfigurator)
