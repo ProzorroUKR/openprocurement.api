ce6c6d1c338824a97f4fb2cd36c11738e3311481
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2017-03-30 12:48:40 +0300

    add configuration adapter

---
+++ openprocurement/tender/openua/adapters.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from openprocurement.tender.core.adapters import TenderConfigurator
+
+
+class TenderAboveThresholdUAConfigurator(TenderConfigurator):
+    """ AboveThresholdUA Tender configuration adapter """
+
+    name = "AboveThresholdUA Tender configurator"
