98ff30e2a7058b48582502de85dd34bab26d5dd3
Iryna <iryna.pomazan@unity-bars.com>
2017-03-30 12:45:02 +0300

    Removed upload_tender_documents permission for patch

--- openprocurement/tender/openeu/views/qualification_document.py
+++ openprocurement/tender/openeu/views/qualification_document.py
@@ -93,7 +93,7 @@ class TenderQualificationDocumentResource(APIResource):
                         extra=context_unpack(self.request, {'MESSAGE_ID': 'tender_qualification_document_put'}))
             return {'data': document.serialize("view")}
 
-    @json_view(content_type="application/json", validators=(validate_patch_document_data,), permission='upload_tender_documents')
+    @json_view(content_type="application/json", validators=(validate_patch_document_data,), permission='edit_tender')
     def patch(self):
         """Tender Qualification Document Update"""
         if self.request.validated['tender_status'] != 'active.pre-qualification':
