85e7b988e836307eff4f60b4226a73bebee9e77c
annawzz <annawzz@quintagroup.org>
2017-03-30 13:24:08 +0300

    Validators refactoring in lot view

--- openprocurement/tender/openuadefense/views/lot.py
+++ openprocurement/tender/openuadefense/views/lot.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from openprocurement.api.utils import get_now
+from openprocurement.api.utils import get_now, error_handler
 from openprocurement.tender.core.utils import optendersresource
 from openprocurement.tender.openua.views.lot import (
     TenderUaLotResource as TenderLotResource
@@ -17,14 +17,10 @@ from openprocurement.tender.openuadefense.constants import (
                    description="Tender Ua lots")
 class TenderUaLotResource(TenderLotResource):
 
-    def validate_update_tender(self, operation):
+    def validate_update_tender(self):
         tender = self.request.validated['tender']
-        if tender.status not in ['active.tendering']:
-            self.request.errors.add('body', 'data', 'Can\'t {} lot in current ({}) tender status'.format(operation, tender.status))
-            self.request.errors.status = 403
-            return
         if calculate_business_date(get_now(), TENDERING_EXTRA_PERIOD, tender, True) > tender.tenderPeriod.endDate:
             self.request.errors.add('body', 'data', 'tenderPeriod should be extended by {0.days} working days'.format(TENDERING_EXTRA_PERIOD))
             self.request.errors.status = 403
-            return
+            raise error_handler(self.request.errors)
         return True
