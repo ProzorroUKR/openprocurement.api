9b4edf363e16d314a1a59387751929c6b4061338
annawzz <annawzz@quintagroup.org>
2017-03-30 19:30:27 +0300

    Add validators

--- openprocurement/tender/limited/validation.py
+++ openprocurement/tender/limited/validation.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from openprocurement.api.validation import validate_data
-from openprocurement.api.utils import update_logging_context, error_handler  # XXX tender context
+from openprocurement.tender.core.validation import OPERATIONS
+from openprocurement.api.utils import update_logging_context, error_handler, get_now  # XXX tender context
 
 
 def validate_complaint_data(request):
@@ -39,6 +40,136 @@ def validate_update_tender_with_awards(request):
 # tender document
 def validate_operation_with_document_not_in_active_status(request):
     if request.validated['tender_status'] != 'active':
-        request.errors.add('body', 'data', 'Can\'t {} document in current ({}) tender status'.format('add' if request.method == 'POST' else 'update', request.validated['tender_status']))
+        request.errors.add('body', 'data', 'Can\'t {} document in current ({}) tender status'.format(OPERATIONS.get(request.method), request.validated['tender_status']))
+        request.errors.status = 403
+        raise error_handler(request.errors)
+
+# lot
+def validate_lot_operation_not_in_active_status(request):
+    tender = request.validated['tender']
+    if tender.status != 'active':
+        request.errors.add('body', 'data', 'Can\'t {} lot in current ({}) tender status'.format(OPERATIONS.get(request.method), tender.status))
+        request.errors.status = 403
+        raise error_handler(request.errors)
+
+
+def validate_lot_operation_with_awards(request):
+    tender = request.validated['tender']
+    if tender.awards:
+        request.errors.add('body', 'data', 'Can\'t {} lot when you have awards'.format(OPERATIONS.get(request.method), tender.status))
+        request.errors.status = 403
+        raise error_handler(request.errors)
+
+# award
+def validate_award_operation_not_in_active_status(request):
+    tender = request.validated['tender']
+    if tender.status != 'active':
+        request.errors.add('body', 'data', 'Can\'t {} award in current ({}) tender status'.format('create' if request.method == 'POST' else 'update', tender.status))
+        request.errors.status = 403
+        raise error_handler(request.errors)
+
+
+def validate_create_new_award(request):
+    tender = request.validated['tender']
+    if tender.awards and tender.awards[-1].status in ['pending', 'active']:
+        request.errors.add('body', 'data', 'Can\'t create new award while any ({}) award exists'.format(tender.awards[-1].status))
+        request.errors.status = 403
+        raise error_handler(request.errors)
+
+
+def validate_lot_cancellation(request):
+    tender = request.validated['tender']
+    award = request.validated['award']
+    if tender.get('lots') and tender.get('cancellations') and [cancellation for cancellation in tender.get('cancellations', []) if cancellation.get('relatedLot') == award.lotID]:
+        request.errors.add('body', 'data', 'Can\'t {} award while cancellation for corresponding lot exists'.format(OPERATIONS.get(request.method)))
+        request.errors.status = 403
+        raise error_handler(request.errors)
+
+
+def validate_create_new_award_with_lots(request):
+    tender = request.validated['tender']
+    award = request.validated['award']
+    if tender.awards:
+        if tender.lots:  # If tender with lots
+            if award.lotID in [aw.lotID for aw in tender.awards if aw.status in ['pending', 'active']]:
+                request.errors.add('body', 'data', 'Can\'t create new award on lot while any ({}) award exists'.format(tender.awards[-1].status))
+                request.errors.status = 403
+                raise error_handler(request.errors)
+        else:
+            validate_create_new_award(request)
+
+# award document
+def validate_document_operation_not_in_active(request):
+    if request.validated['tender_status'] != 'active':
+        request.errors.add('body', 'data', 'Can\'t {} document in current ({}) tender status'.format(OPERATIONS.get(request.method), request.validated['tender_status']))
+        request.errors.status = 403
+        raise error_handler(request.errors)
+
+
+def validate_award_document_add_not_in_pending(request):
+    if request.validated['award'].status != 'pending':
+        request.errors.add('body', 'data', 'Can\'t add document in current ({}) award status'.format(request.validated['award'].status))
+        request.errors.status = 403
+        raise error_handler(request.errors)
+
+# award complaint
+def validate_award_complaint_operation_not_in_active(request):
+    tender = request.validated['tender']
+    if tender.status != 'active':
+        request.errors.add('body', 'data', 'Can\'t {} complaint in current ({}) tender status'.format(OPERATIONS.get(request.method), tender.status))
+        request.errors.status = 403
+        raise error_handler(request.errors)
+
+# contract
+def validate_contract_operation_not_in_active(request):
+    if request.validated['tender_status'] not in ['active']:
+        request.errors.add('body', 'data', 'Can\'t {} contract in current ({}) tender status'.format(OPERATIONS.get(request.method), request.validated['tender_status']))
+        request.errors.status = 403
+        raise error_handler(request.errors)
+
+
+def validate_contract_update_in_cancelled(request):
+    if request.context.status == 'cancelled':
+        request.errors.add('body', 'data', 'Can\'t update contract in current ({}) status'.format(request.context.status))
+        request.errors.status = 403
+        raise error_handler(request.errors)
+
+
+def validate_contract_with_cancellations_and_contract_signing(request):
+    data = request.validated['data']
+    if request.context.status != 'active' and 'status' in data and data['status'] == 'active':
+        tender = request.validated['tender']
+        award = [a for a in tender.awards if a.id == request.context.awardID][0]
+        if tender.get('lots') and tender.get('cancellations') and [cancellation for cancellation in tender.get('cancellations') if cancellation.get('relatedLot') == award.lotID]:
+            request.errors.add('body', 'data', 'Can\'t update contract while cancellation for corresponding lot exists')
+            request.errors.status = 403
+            raise error_handler(request.errors)
+        stand_still_end = award.complaintPeriod.endDate
+        if stand_still_end > get_now():
+            request.errors.add('body', 'data', 'Can\'t sign contract before stand-still period end ({})'.format(stand_still_end.isoformat()))
+            request.errors.status = 403
+            raise error_handler(request.errors)
+        if any([
+            i.status in tender.block_complaint_status and a.lotID == award.lotID
+            for a in tender.awards
+            for i in a.complaints
+        ]):
+            request.errors.add('body', 'data', 'Can\'t sign contract before reviewing all complaints')
+            request.errors.status = 403
+            raise error_handler(request.errors)
+
+# contract document
+def validate_contract_document_operation_not_in_allowed_contract_status(request):
+    contract = request.validated['contract']
+    if contract.status not in ['pending', 'active']:
+        request.errors.add('body', 'data', 'Can\'t {} document in current contract status'.format(OPERATIONS.get(request.method)))
+        request.errors.status = 403
+        raise error_handler(request.errors)
+
+#cancellation
+def validate_cancellation_in_termainated_status(request):
+    tender = request.validated['tender']
+    if tender.status in ['complete', 'cancelled', 'unsuccessful']:
+        request.errors.add('body', 'data', 'Can\'t {} cancellation in current ({}) tender status'.format(OPERATIONS.get(request.method), tender.status))
         request.errors.status = 403
         raise error_handler(request.errors)
