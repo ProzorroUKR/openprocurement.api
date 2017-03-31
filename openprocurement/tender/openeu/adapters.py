591b3d5a0463db02b85a71aa6969dbe8e0afe597
annawzz <annawzz@quintagroup.org>
2017-03-31 15:59:36 +0300

    Add tendering_period_extra constant

--- openprocurement/tender/openeu/adapters.py
+++ openprocurement/tender/openeu/adapters.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 from openprocurement.tender.core.adapters import TenderConfigurator
 from openprocurement.tender.openeu.models import Tender
+from openprocurement.tender.openua.constants import (
+    TENDERING_EXTRA_PERIOD
+)
 from openprocurement.tender.openeu.constants import (
     TENDERING_DURATION, PREQUALIFICATION_COMPLAINT_STAND_STILL
 )
+
 
 
 class TenderAboveThresholdEUConfigurator(TenderConfigurator):
@@ -14,6 +18,9 @@ class TenderAboveThresholdEUConfigurator(TenderConfigurator):
 
     # duration of tendering period. timedelta object.
     tendering_period_duration = TENDERING_DURATION
+
+    # duration of tender period extension. timedelta object
+    tendering_period_extra = TENDERING_EXTRA_PERIOD
 
     # duration of pre-qualification stand-still period. timedelta object.
     prequalification_complaint_stand_still = PREQUALIFICATION_COMPLAINT_STAND_STILL
