03e9edb61dd76e0aea384b6b363d80e810e9dec4
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2017-03-31 11:45:39 +0300

    add accreditation properties

--- openprocurement/tender/core/adapters.py
+++ openprocurement/tender/core/adapters.py
@@ -6,3 +6,14 @@ class TenderConfigurator(ContentConfigurator):
     """ Tender configuration adapter """
 
     name = "Tender Configurator"
+    model = None
+
+    @property
+    def create_accreditation(self):
+        """ tender create accreditation level """
+        return self.model.create_accreditation
+
+    @property
+    def edit_accreditation(self):
+        """ bid create accreditation level """
+        return self.model.edit_accreditation
