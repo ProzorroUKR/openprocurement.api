f16d66772914fc229f530ca0ec4d2afa55b45af3
annawzz <annawzz@quintagroup.org>
2017-03-31 16:00:41 +0300

    Update validators in tender view

--- openprocurement/tender/openeu/views/tender.py
+++ openprocurement/tender/openeu/views/tender.py
@@ -13,7 +13,8 @@ from openprocurement.tender.core.utils import (
 )
 from openprocurement.tender.core.validation import (
     validate_tender_status_update_in_terminated_status,
-    validate_tender_status_update_not_in_pre_qualificaton
+    validate_tender_status_update_not_in_pre_qualificaton,
+    validate_tender_period_extension_by_owner_in_tendering
 )
 from openprocurement.tender.belowthreshold.views.tender import TenderResource
 from openprocurement.tender.openeu.utils import (
@@ -25,9 +26,6 @@ from openprocurement.tender.openua.validation import validate_patch_tender_ua_da
 from openprocurement.tender.openeu.constants import (
     PREQUALIFICATION_COMPLAINT_STAND_STILL as COMPLAINT_STAND_STILL
 )
-from openprocurement.tender.openua.constants import (
-    TENDERING_EXTRA_PERIOD
-)
 from openprocurement.tender.core.events import TenderInitializeEvent
 
 @optendersresource(name='aboveThresholdEU:Tender',
@@ -38,7 +36,7 @@ class TenderEUResource(TenderResource):
     """ Resource handler for TenderEU """
 
     @json_view(content_type="application/json", validators=(validate_patch_tender_ua_data, validate_tender_status_update_in_terminated_status,
-               validate_tender_status_update_not_in_pre_qualificaton, ), permission='edit_tender')
+               validate_tender_status_update_not_in_pre_qualificaton, validate_tender_period_extension_by_owner_in_tendering), permission='edit_tender')
     def patch(self):
         """Tender Edit (partial)
 
@@ -88,19 +86,9 @@ class TenderEUResource(TenderResource):
 
         """
         tender = self.context
-        data = self.request.validated['data']
 
-        # TODO use tender configurator instead of TENDERING_EXTRA_PERIOD
-        if self.request.authenticated_role == 'tender_owner' and self.request.validated['tender_status'] == 'active.tendering':
-            if 'tenderPeriod' in data and 'endDate' in data['tenderPeriod']:
-                self.request.validated['tender'].tenderPeriod.import_data(data['tenderPeriod'])
-                if calculate_business_date(get_now(), TENDERING_EXTRA_PERIOD, self.request.validated['tender']) > self.request.validated['tender'].tenderPeriod.endDate:
-                    self.request.errors.add('body', 'data', 'tenderPeriod should be extended by {0.days} days'.format(TENDERING_EXTRA_PERIOD))
-                    self.request.errors.status = 403
-                    raise error_handler(self.request.errors)
-                self.request.registry.notify(TenderInitializeEvent(self.request.validated['tender']))
-                self.request.validated['data']["enquiryPeriod"] = self.request.validated['tender'].enquiryPeriod.serialize()
-
+        self.request.registry.notify(TenderInitializeEvent(self.request.validated['tender']))
+        self.request.validated['data']["enquiryPeriod"] = self.request.validated['tender'].enquiryPeriod.serialize()
         apply_patch(self.request, save=False, src=self.request.validated['tender_src'])
         if self.request.authenticated_role == 'chronograph':
             check_status(self.request)
