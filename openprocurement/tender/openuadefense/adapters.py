772b3526b407e85f210d809fc7b8731306ea6a2a
annawzz <annawzz@quintagroup.org>
2017-04-01 16:25:15 +0300

    Add contstants to configurator

--- openprocurement/tender/openuadefense/adapters.py
+++ openprocurement/tender/openuadefense/adapters.py
@@ -3,6 +3,10 @@ from openprocurement.tender.openua.adapters import (
     TenderAboveThresholdUAConfigurator
 )
 from openprocurement.tender.openuadefense.models import Tender
+from openprocurement.tender.openuadefense.constants import (
+    TENDERING_EXTRA_PERIOD,
+    CLAIM_SUBMIT_TIME, COMPLAINT_SUBMIT_TIME
+)
 
 
 class TenderAboveThresholdUADefConfigurator(TenderAboveThresholdUAConfigurator):
@@ -10,3 +14,12 @@ class TenderAboveThresholdUADefConfigurator(TenderAboveThresholdUAConfigurator):
 
     name = "AboveThresholdUA Defense Tender configurator"
     model = Tender
+
+    # Duration of tender period extension. timedelta object
+    tendering_period_extra = TENDERING_EXTRA_PERIOD
+
+    # Tender claims should be sumbitted not later then "tender_claim_submit_time" days before tendering period end. Timedelta object
+    tender_claim_submit_time =CLAIM_SUBMIT_TIME
+
+    # Tender complaints should be sumbitted not later then "tender_claim_submit_time" days before tendering period end. Timedelta object
+    tender_complaint_submit_time = COMPLAINT_SUBMIT_TIME
