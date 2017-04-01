c91e78aad0550ff68a161e5f6169a05e7a736988
annawzz <annawzz@quintagroup.org>
2017-04-01 17:24:16 +0300

    Add constants to configurator

--- openprocurement/tender/openeu/adapters.py
+++ openprocurement/tender/openeu/adapters.py
@@ -2,7 +2,8 @@
 from openprocurement.tender.core.adapters import TenderConfigurator
 from openprocurement.tender.openeu.models import Tender
 from openprocurement.tender.openua.constants import (
-    TENDERING_EXTRA_PERIOD, STATUS4ROLE
+    TENDERING_EXTRA_PERIOD, STATUS4ROLE,
+    CLAIM_SUBMIT_TIME, COMPLAINT_SUBMIT_TIME
 )
 from openprocurement.tender.openeu.constants import (
     TENDERING_DURATION, PREQUALIFICATION_COMPLAINT_STAND_STILL
@@ -29,3 +30,9 @@ class TenderAboveThresholdEUConfigurator(TenderConfigurator):
 
     # Dictionary with allowed complaint statuses for operations for each role
     allowed_statuses_for_complaint_operations_for_roles = STATUS4ROLE
+
+    # Tender claims should be sumbitted not later then "tender_claim_submit_time" days before tendering period end. Timedelta object
+    tender_claim_submit_time = CLAIM_SUBMIT_TIME
+
+    # Tender complaints should be sumbitted not later then "tender_claim_submit_time" days before tendering period end. Timedelta object
+    tender_complaint_submit_time = COMPLAINT_SUBMIT_TIME
