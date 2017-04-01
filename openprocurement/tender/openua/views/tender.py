83b3238c0eb486e358f940c3433f0f7fbeeccc59
annawzz <annawzz@quintagroup.org>
2017-04-01 15:49:47 +0300

    Validators refactoring for tender view

--- openprocurement/tender/openua/views/tender.py
+++ openprocurement/tender/openua/views/tender.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
-from openprocurement.tender.core.validation import validate_tender_status_update_in_terminated_status
+from openprocurement.tender.core.validation import (
+    validate_tender_period_extension,
+    validate_tender_status_update_in_terminated_status
+)
 from openprocurement.tender.belowthreshold.views.tender import TenderResource
 from openprocurement.tender.openua.validation import validate_patch_tender_ua_data
 from openprocurement.tender.openua.utils import (
@@ -79,14 +82,10 @@ class TenderUAResource(TenderResource):
         tender = self.context
         data = self.request.validated['data']
 
-        # TODO use tender configurator instead of TENDERING_EXTRA_PERIOD
         if self.request.authenticated_role == 'tender_owner' and self.request.validated['tender_status'] == 'active.tendering':
             if 'tenderPeriod' in data and 'endDate' in data['tenderPeriod']:
                 self.request.validated['tender'].tenderPeriod.import_data(data['tenderPeriod'])
-                if calculate_business_date(get_now(), TENDERING_EXTRA_PERIOD, context=tender) > self.request.validated['tender'].tenderPeriod.endDate:
-                    self.request.errors.add('body', 'data', 'tenderPeriod should be extended by {0.days} days'.format(TENDERING_EXTRA_PERIOD))
-                    self.request.errors.status = 403
-                    raise error_handler(self.request.errors)
+                validate_tender_period_extension(self.request)
                 self.request.registry.notify(TenderInitializeEvent(self.request.validated['tender']))
                 self.request.validated['data']["enquiryPeriod"] = self.request.validated['tender'].enquiryPeriod.serialize()
 
