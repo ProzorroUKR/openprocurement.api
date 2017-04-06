1fa941f39856c4c46d5c7e90e32e7699e11f5e71
annawzz <annawzz@quintagroup.org>
2017-04-06 18:02:20 +0300

    Update imports and validators

--- openprocurement/tender/openua/validation.py
+++ openprocurement/tender/openua/validation.py
@@ -1,5 +1,5 @@
-from openprocurement.api.validation import validate_data, validate_json_data
-from openprocurement.api.utils import apply_data_patch, error_handler, get_now
+from openprocurement.api.validation import validate_data, validate_json_data, OPERATIONS
+from openprocurement.api.utils import apply_data_patch, error_handler, get_now, raise_operation_error
 from openprocurement.tender.core.utils import calculate_business_date
 
 
@@ -9,9 +9,7 @@ def validate_patch_tender_ua_data(request):
     if request.context.status == 'draft':
         default_status = type(request.tender).fields['status'].default
         if data and data.get('status') != default_status:
-            request.errors.add('body', 'data', 'Can\'t update tender in current (draft) status')
-            request.errors.status = 403
-            raise error_handler(request.errors)
+            raise_operation_error(request, 'Can\'t update tender in current (draft) status')
         request.validated['data'] = {'status': default_status}
         request.context.status = default_status
         return
@@ -55,9 +53,7 @@ def validate_submit_claim_time(request):
     tender = request.context
     claim_submit_time = request.content_configurator.tender_claim_submit_time
     if get_now() > calculate_business_date(tender.tenderPeriod.endDate, -claim_submit_time, tender):
-        request.errors.add('body', 'data', 'Can submit claim not later than {0.days} days before tenderPeriod end'.format(claim_submit_time))
-        request.errors.status = 403
-        raise error_handler(request.errors)
+        raise_operation_error(request, 'Can submit claim not later than {0.days} days before tenderPeriod end'.format(claim_submit_time))
 
 # complaint documents
 def validate_complaint_author(request):
@@ -69,14 +65,10 @@ def validate_complaint_author(request):
 
 def validate_complaint_document_operation_not_in_allowed_status(request):
     if request.validated['tender_status'] not in ['active.tendering']:
-        request.errors.add('body', 'data', 'Can\'t {} document in current ({}) tender status'.format('add' if request.method == 'POST' else 'update', request.validated['tender_status']))
-        request.errors.status = 403
-        raise error_handler(request.errors)
+        raise_operation_error(request, 'Can\'t {} document in current ({}) tender status'.format(OPERATIONS.get(request.method), request.validated['tender_status']))
 
 # contract
 def validate_contract_update_with_accepted_complaint(request):
     tender = request.validated['tender']
     if any([any([c.status == 'accepted' for c in i.complaints]) for i in tender.awards if i.lotID in [a.lotID for a in tender.awards if a.id == request.context.awardID]]):
-        request.errors.add('body', 'data', 'Can\'t update contract with accepted complaint')
-        request.errors.status = 403
-        raise error_handler(request.errors)
+        raise_operation_error(request, 'Can\'t update contract with accepted complaint')
