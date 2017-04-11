2a29fc443feca497241462b1c83a1d3b8e05faf6
Ihor Vlasiuk <scandie@quintagroup.org>
2017-04-11 15:02:08 +0300

    create basic abstract classes for document module

--- openprocurement/tender/limited/tests/document.py
+++ openprocurement/tender/limited/tests/document.py
@@ -1,19 +1,9 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from openprocurement.api.tests.base import snitch
-
-
-from openprocurement.tender.belowthreshold.tests.document_blanks import (
-    # TenderDocument ResourceTest
-    not_found,
-    create_tender_document,
-    put_tender_document,
-    patch_tender_document,
-    # TenderDocumentResourceTest
-    create_tender_document_json_invalid,
-    create_tender_document_json,
-    put_tender_document_json,
+from openprocurement.tender.belowthreshold.tests.document import (
+    TenderDocumentResourceTestMixin,
+    TenderDocumentWithDSResourceTestMixin
 )
 
 from openprocurement.tender.limited.tests.base import (
@@ -24,15 +14,10 @@ from openprocurement.tender.limited.tests.base import (
 )
 
 
-class TenderDocumentResourceTest(BaseTenderContentWebTest):
+class TenderDocumentResourceTest(BaseTenderContentWebTest, TenderDocumentResourceTestMixin):
     initial_data = test_tender_data
     docservice = False
     test_forbidden_document_actions_status = "complete"
-
-    test_not_found = snitch(not_found)
-    test_create_tender_document = snitch(create_tender_document)
-    test_put_tender_document = snitch(put_tender_document)
-    test_patch_tender_document = snitch(patch_tender_document)
 
 
 class TenderNegotiationDocumentResourceTest(TenderDocumentResourceTest):
@@ -43,12 +28,8 @@ class TenderNegotiationQuickDocumentResourceTest(TenderNegotiationDocumentResour
     initial_data = test_tender_negotiation_quick_data
 
 
-class TenderDocumentWithDSResourceTest(TenderDocumentResourceTest):
+class TenderDocumentWithDSResourceTest(TenderDocumentResourceTest, TenderDocumentWithDSResourceTestMixin):
     docservice = True
-
-    test_create_tender_document_json_invalid = snitch(create_tender_document_json_invalid)
-    test_create_tender_document_json = snitch(create_tender_document_json)
-    test_put_tender_document_json = snitch(put_tender_document_json)
 
 
 class TenderNegotiationDocumentWithDSResourceTest(TenderDocumentWithDSResourceTest):
