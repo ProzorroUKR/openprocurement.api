88c0bfde36bbd16bcbf6203f8be3201a8de4cea8
Ihor Vlasiuk <scandie@quintagroup.org>
2017-04-11 11:51:16 +0300

    create basic abstract classes for document module

--- openprocurement/tender/belowthreshold/tests/document.py
+++ openprocurement/tender/belowthreshold/tests/document.py
@@ -21,23 +21,29 @@ from openprocurement.tender.belowthreshold.tests.document_blanks import (
 )
 
 
-class TenderDocumentResourceTest(TenderContentWebTest):
-    test_forbidden_document_actions_status = 'active.tendering'
-
+class TenderDocumentResourceTestMixin(object):
     test_not_found = snitch(not_found)
-    test_create_document_active_tendering_status = snitch(create_document_active_tendering_status)
     test_create_tender_document = snitch(create_tender_document)
     test_put_tender_document = snitch(put_tender_document)
     test_patch_tender_document = snitch(patch_tender_document)
 
 
-class TenderDocumentWithDSResourceTest(TenderDocumentResourceTest):
-
-    docservice = True
-    test_create_tender_document_error = snitch(create_tender_document_error)
+class TenderDocumentWithDSResourceTestMixin(object):
     test_create_tender_document_json_invalid = snitch(create_tender_document_json_invalid)
     test_create_tender_document_json = snitch(create_tender_document_json)
     test_put_tender_document_json = snitch(put_tender_document_json)
+
+
+class TenderDocumentResourceTest(TenderContentWebTest, TenderDocumentResourceTestMixin):
+    test_forbidden_document_actions_status = 'active.tendering'
+
+    test_create_document_active_tendering_status = snitch(create_document_active_tendering_status)
+
+
+class TenderDocumentWithDSResourceTest(TenderDocumentResourceTest, TenderDocumentWithDSResourceTestMixin):
+
+    docservice = True
+    test_create_tender_document_error = snitch(create_tender_document_error)
 
 
 def suite():
