120a2675d1bf3b7df9175bd4cfa2351c4d749e10
annawzz <annawzz@quintagroup.org>
2017-04-25 11:34:03 +0300

    Update validators

--- openprocurement/tender/openua/views/contract.py
+++ openprocurement/tender/openua/views/contract.py
@@ -8,6 +8,7 @@ from openprocurement.api.utils import (
 )
 from openprocurement.tender.openua.utils import check_tender_status
 from openprocurement.tender.core.validation import (
+    validate_contract_signing,
     validate_patch_contract_data,
     validate_update_contract_value,
     validate_update_contract_only_for_active_lots,
@@ -29,31 +30,10 @@ from openprocurement.tender.openua.validation import validate_contract_update_wi
 class TenderUaAwardContractResource(TenderAwardContractResource):
 
     @json_view(content_type="application/json", permission='edit_tender', validators=(validate_patch_contract_data, validate_contract_operation_not_in_allowed_status,
-               validate_update_contract_only_for_active_lots, validate_contract_update_with_accepted_complaint, validate_update_contract_value))
+               validate_update_contract_only_for_active_lots, validate_contract_update_with_accepted_complaint, validate_update_contract_value, validate_contract_signing))
     def patch(self):
         """Update of contract
         """
-        tender = self.request.validated['tender']
-        data = self.request.validated['data']
-
-        if self.request.context.status != 'active' and 'status' in data and data['status'] == 'active':
-            award = [a for a in tender.awards if a.id == self.request.context.awardID][0]
-            stand_still_end = award.complaintPeriod.endDate
-            if stand_still_end > get_now():
-                raise_operation_error(self.request, 'Can\'t sign contract before stand-still period end ({})'.format(stand_still_end.isoformat()))
-            pending_complaints = [
-                i
-                for i in tender.complaints
-                if i.status in tender.block_complaint_status and i.relatedLot in [None, award.lotID]
-            ]
-            pending_awards_complaints = [
-                i
-                for a in tender.awards
-                for i in a.complaints
-                if i.status in tender.block_complaint_status and a.lotID == award.lotID
-            ]
-            if pending_complaints or pending_awards_complaints:
-                raise_operation_error(self.request, 'Can\'t sign contract before reviewing all complaints')
         contract_status = self.request.context.status
         apply_patch(self.request, save=False, src=self.request.context.serialize())
         if contract_status != self.request.context.status and (contract_status != 'pending' or self.request.context.status != 'active'):
