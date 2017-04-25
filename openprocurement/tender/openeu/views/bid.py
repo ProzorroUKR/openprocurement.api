4114bba19537bef38df3559bea7cb53ced34e81a
annawzz <annawzz@quintagroup.org>
2017-04-25 11:46:55 +0300

    Add raise error_handler

--- openprocurement/tender/openeu/views/bid.py
+++ openprocurement/tender/openeu/views/bid.py
@@ -101,7 +101,6 @@ class TenderBidResource(BaseResource):
         """
         if self.request.authenticated_role == 'bid_owner':
             return {'data': self.request.context.serialize('view')}
-        # TODO can't move this validator becacuse of check above
         validate_view_bids_in_active_tendering(self.request)
         return {'data': self.request.context.serialize(self.request.validated['tender_status'])}
 
