5163b57b71cbeaac08d36e36ff4522b2ff6215d1
annawzz <annawzz@quintagroup.org>
2017-04-26 18:43:11 +0300

    Remove imports

--- openprocurement/tender/openeu/views/auction.py
+++ openprocurement/tender/openeu/views/auction.py
@@ -1,16 +1,6 @@
 # -*- coding: utf-8 -*-
-from openprocurement.api.utils import (
-    json_view,
-    context_unpack
-)
-from openprocurement.tender.core.utils import (
-    optendersresource,
-    apply_patch,
-    save_tender
-)
-from openprocurement.tender.core.validation import validate_tender_auction_data
+from openprocurement.tender.core.utils import optendersresource
 from openprocurement.tender.openua.views.auction import TenderUaAuctionResource as BaseResource
-from openprocurement.tender.openua.utils import add_next_award
 
 
 @optendersresource(name='aboveThresholdEU:Tender Auction',
