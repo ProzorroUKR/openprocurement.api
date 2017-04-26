9f2219f7c02f1ddd9d122c6115e331e6f6329d20
annawzz <annawzz@quintagroup.org>
2017-04-26 13:31:11 +0300

    Update imports

--- openprocurement/tender/belowthreshold/validation.py
+++ openprocurement/tender/belowthreshold/validation.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-from openprocurement.api.utils import update_logging_context, error_handler, raise_operation_error
+from openprocurement.api.utils import update_logging_context, error_handler, raise_operation_error, check_document
 from openprocurement.api.validation import validate_data, OPERATIONS
-from openprocurement.tender.belowthreshold.utils import  check_document
 
 
 # tender documents
