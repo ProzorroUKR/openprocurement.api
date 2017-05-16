b5bef04b4168e35feba03081a0800bccdf57171d
Scandie <grizly.vl@gmail.com>
2017-05-16 12:02:55 +0300

    add tests for NBUdiscountRate field

--- openprocurement/tender/esco/tests/tender_blanks.py
+++ openprocurement/tender/esco/tests/tender_blanks.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
+from copy import deepcopy
 from openprocurement.tender.esco.models import TenderESCOEU
-from openprocurement.tender.esco.tests.base import (
-    test_tender_eu_data
-)
 
 
 # TenderESCOEUTest
 
 
 def simple_add_tender(self):
-    u = TenderESCOEU(test_tender_eu_data)
+    u = TenderESCOEU(self.initial_data)
     u.tenderID = "UA-X"
 
     assert u.id is None
@@ -28,3 +26,61 @@ def simple_add_tender(self):
     assert fromdb['procurementMethodType'] == "esco.EU"
 
     u.delete_instance(self.db)
+
+
+def tender_with_nbu_discount_rate(self):
+    data = deepcopy(self.initial_data)
+    del data['NBUdiscountRate']
+    response = self.app.post_json('/tenders', {'data': data}, status=422)
+    self.assertEqual(response.status, '422 Unprocessable Entity')
+    self.assertEqual(response.content_type, 'application/json')
+    self.assertEqual(response.json['status'], 'error')
+    self.assertEqual(response.json['errors'], [
+        {u'description': [u'This field is required.'],
+         u'location': u'body', u'name': u'NBUdiscountRate'}
+    ])
+
+    data = deepcopy(self.initial_data)
+    data.update({'id': 'hash', 'doc_id': 'hash2', 'tenderID': 'hash3'})
+    response = self.app.post_json('/tenders', {'data': data})
+    self.assertEqual(response.status, '201 Created')
+    self.assertEqual(response.content_type, 'application/json')
+    tender = response.json['data']
+    owner_token = response.json['access']['token']
+    if 'procurementMethodDetails' in tender:
+        tender.pop('procurementMethodDetails')
+    self.assertEqual(set(tender), set([
+        u'procurementMethodType', u'id', u'dateModified', u'tenderID',
+        u'status', u'enquiryPeriod', u'tenderPeriod', u'auctionPeriod',
+        u'complaintPeriod', u'minimalStep', u'items', u'value', u'owner',
+        u'procuringEntity', u'next_check', u'procurementMethod',
+        u'awardCriteria', u'submissionMethod', u'title', u'title_en',  u'date', u'NBUdiscountRate']))
+    self.assertNotEqual(data['id'], tender['id'])
+    self.assertNotEqual(data['doc_id'], tender['id'])
+    self.assertNotEqual(data['tenderID'], tender['tenderID'])
+
+    response = self.app.patch_json('/tenders/{}?acc_token={}'.format(tender['id'], owner_token),
+                                   {"data": {"NBUdiscountRate": 1.2}}, status=422)
+    self.assertEqual(response.status, '422 Unprocessable Entity')
+    self.assertEqual(response.content_type, 'application/json')
+    self.assertEqual(response.json['status'], 'error')
+    self.assertEqual(response.json['errors'], [
+        {u'description': [u'Float value should be less than 0.99.'],
+         u'location': u'body', u'name': u'NBUdiscountRate'}
+    ])
+
+    response = self.app.patch_json('/tenders/{}?acc_token={}'.format(tender['id'], owner_token),
+                                   {"data": {"NBUdiscountRate": -2}}, status=422)
+    self.assertEqual(response.status, '422 Unprocessable Entity')
+    self.assertEqual(response.content_type, 'application/json')
+    self.assertEqual(response.json['status'], 'error')
+    self.assertEqual(response.json['errors'], [
+        {u'description': [u'Float value should be greater than 0.'],
+         u'location': u'body', u'name': u'NBUdiscountRate'}
+    ])
+
+    response = self.app.patch_json('/tenders/{}?acc_token={}'.format(tender['id'], owner_token),
+                                   {"data": {"NBUdiscountRate": 0.3}})
+    self.assertEqual(response.status, '200 OK')
+    self.assertIn('NBUdiscountRate', response.json['data'])
+    self.assertEqual(response.json['data']['NBUdiscountRate'], 0.3)
