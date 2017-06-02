e4ed53277156e7f7eacd4e8f720da4851c7740c7
Scandie <grizly.vl@gmail.com>
2017-06-02 12:57:51 +0300

    Revert contract changes
    This reverts commits 2b49815 and 993f09c

--- openprocurement/tender/esco/views/contract.py
+++ openprocurement/tender/esco/views/contract.py
@@ -1,32 +1,6 @@
 # -*- coding: utf-8 -*-
-from openprocurement.api.utils import (
-    get_now,
-    json_view,
-    context_unpack,
-    raise_operation_error
-)
-
-from openprocurement.tender.core.utils import (
-    save_tender, optendersresource, apply_patch,
-)
-from openprocurement.tender.core.validation import (
-    validate_contract_signing,
-    validate_patch_contract_data,
-    validate_update_contract_only_for_active_lots,
-    validate_contract_operation_not_in_allowed_status
-)
-
-from openprocurement.tender.belowthreshold.utils import (
-    check_tender_status,
-)
-
-from openprocurement.tender.openua.validation import validate_contract_update_with_accepted_complaint
-
 from openprocurement.tender.openeu.views.contract import TenderAwardContractResource as TenderEUContractResource
-
-from openprocurement.tender.esco.validation import (
-    validate_update_contract_value,
-)
+from openprocurement.tender.core.utils import optendersresource
 
 
 @optendersresource(name='esco.EU:Tender Contracts',
@@ -36,20 +10,3 @@ from openprocurement.tender.esco.validation import (
                    description="Tender ESCO EU contracts")
 class TenderESCOEUContractResource(TenderEUContractResource):
     """ Tender ESCO EU Contract Resource """
-
-    @json_view(content_type="application/json", permission='edit_tender', validators=(validate_patch_contract_data, validate_contract_operation_not_in_allowed_status,
-               validate_update_contract_only_for_active_lots, validate_contract_update_with_accepted_complaint, validate_update_contract_value, validate_contract_signing))
-    def patch(self):
-        """Update of contract
-        """
-        contract_status = self.request.context.status
-        apply_patch(self.request, save=False, src=self.request.context.serialize())
-        if contract_status != self.request.context.status and (contract_status != 'pending' or self.request.context.status != 'active'):
-            raise_operation_error(self.request, 'Can\'t update contract status')
-        if self.request.context.status == 'active' and not self.request.context.dateSigned:
-            self.request.context.dateSigned = get_now()
-        check_tender_status(self.request)
-        if save_tender(self.request):
-            self.LOGGER.info('Updated tender contract {}'.format(self.request.context.id),
-                        extra=context_unpack(self.request, {'MESSAGE_ID': 'tender_contract_patch'}))
-            return {'data': self.request.context.serialize()}
