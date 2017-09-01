5fbbe78a6351a31cc2a4cf94e6cbbcecb9961693
Tuoris <tuoris.dev@gmail.com>
2017-09-01 14:31:43 +0300

    fixup! Fix typo at TenderESCOConfigurator

--- openprocurement/tender/esco/adapters.py
+++ openprocurement/tender/esco/adapters.py
@@ -4,9 +4,9 @@ from openprocurement.tender.esco.models import Tender
 
 
 class TenderESCOConfigurator(TenderAboveThresholdEUConfigurator):
-    """ AboveThresholdEU Tender configuration adapter """
+    """ ESCO Tender configuration adapter """
 
-    name = "ESCO Tender configurator"
+    name = "esco Tender configurator"
     model = Tender
 
     # Param to configure award criteria - awards are generated from higher to lower by value.amount
