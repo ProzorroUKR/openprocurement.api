d045aa705b1ab8796a1d99172b033d6437d5c4bc
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2017-09-12 16:40:06 +0300

    j708 fix bids validation. ref: openeu cf0835b1bbefec4f80fc9d7930932e62fb1134e1

--- openprocurement/tender/core/models.py
+++ openprocurement/tender/core/models.py
@@ -162,7 +162,7 @@ def bids_validation_wrapper(validation_func):
             # in case this validation wrapper is used for subelement of bid (such as parameters)
             # traverse back to the bid to get possibility to check status  # troo-to-to =)
             data = data['__parent__']
-        if data['status'] in ('deleted', 'invalid', 'draft'):
+        if data['status'] in ('deleted', 'invalid', 'invalid.pre-qualification', 'draft'):
             # skip not valid bids
             return
         tender = data['__parent__']
