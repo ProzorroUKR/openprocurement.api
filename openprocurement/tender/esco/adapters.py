372ff487c068da2b31cd25e550e8dcd7bd12d17d
annawzz <annawzz@quintagroup.org>
2017-09-19 17:26:37 +0300

    Add awarding criteria field to configurator

--- openprocurement/tender/esco/adapters.py
+++ openprocurement/tender/esco/adapters.py
@@ -11,3 +11,6 @@ class TenderESCOConfigurator(TenderAboveThresholdEUConfigurator):
 
     # Param to configure award criteria - awards are generated from higher to lower by value.amount
     reverse_awarding_criteria = True
+
+    # Param to set awarding criteria field
+    awarding_criteria_key = 'amountPerfomance'
