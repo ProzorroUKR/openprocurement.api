1d766f195c651c466f552f7997749500c833ee22
annawzz <annawzz@quintagroup.org>
2017-09-20 19:15:29 +0300

    Add defaults to fundingKind and yearlyPaymentsPercentageRange

--- openprocurement/tender/esco/models.py
+++ openprocurement/tender/esco/models.py
@@ -98,8 +98,8 @@ class Lot(BaseLot):
     auctionPeriod = ModelType(LotAuctionPeriod, default={})
     auctionUrl = URLType()
     guarantee = ModelType(Guarantee)
-    fundingKind = StringType(choices=['budget', 'other'])
-    yearlyPaymentsPercentageRange = FloatType(required=True)
+    fundingKind = StringType(choices=['budget', 'other'], required=True, default='other')
+    yearlyPaymentsPercentageRange = FloatType(required=True, default=0.8)
 
     @serializable
     def numberOfBids(self):
@@ -326,8 +326,8 @@ class Tender(BaseTender):
     status = StringType(choices=['draft', 'active.tendering', 'active.pre-qualification', 'active.pre-qualification.stand-still', 'active.auction',
                                  'active.qualification', 'active.awarded', 'complete', 'cancelled', 'unsuccessful'], default='active.tendering')
     NBUdiscountRate = FloatType(required=True, min_value=0, max_value=0.99)
-    fundingKind = StringType(choices=['budget', 'other'])
-    yearlyPaymentsPercentageRange = FloatType(required=True)
+    fundingKind = StringType(choices=['budget', 'other'], required=True, default='other')
+    yearlyPaymentsPercentageRange = FloatType(required=True, default=0.8)
     submissionMethodDetails = StringType(default="quick(mode:no-auction)")  # TODO: temporary decision, while esco auction is not ready. Remove after adding auction. Remove function "check_submission_method_details" in openprocurement.tender.esco.subscribers
     announcementDate = datetime(2017, 9, 19) # XXX
 
