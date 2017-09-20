f10753e6e4e52dfc230e51aa471f653e5b337661
annawzz <annawzz@quintagroup.org>
2017-09-20 15:27:01 +0300

    Fix lot fundingKind serialization

--- openprocurement/tender/esco/models.py
+++ openprocurement/tender/esco/models.py
@@ -119,8 +119,7 @@ class Lot(BaseLot):
 
     @serializable(serialized_name="fundingKind")
     def lot_fundingKind(self):
-        if self.fundingKind:
-            return self.__parent__.fundingKind if self.__parent__.fundingKind else self.fundingKind
+        return self.__parent__.fundingKind if self.__parent__.fundingKind else self.fundingKind
 
     @serializable(serialized_name="minimalStep", type=ModelType(Value), serialize_when_none=False)
     def lot_minimalStep(self):
