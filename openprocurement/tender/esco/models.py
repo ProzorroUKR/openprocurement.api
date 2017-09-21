128471808a107fa4014e4bfa1be82483eb3ed802
annawzz <annawzz@quintagroup.org>
2017-09-21 17:44:25 +0300

    Change validation range for features

--- openprocurement/tender/esco/models.py
+++ openprocurement/tender/esco/models.py
@@ -524,12 +524,12 @@ class Tender(BaseTender):
                 i
                 for i in features
                 if i.featureOf == 'tenderer' or i.featureOf == 'lot' and i.relatedItem == lot['id'] or i.featureOf == 'item' and i.relatedItem in [j.id for j in data['items'] if j.relatedLot == lot['id']]
-            ]), 15) > 0.3
+            ]), 15) > 0.25
             for lot in data['lots']
         ]):
-            raise ValidationError(u"Sum of max value of all features for lot should be less then or equal to 30%")
-        elif features and not data['lots'] and round(vnmax(features), 15) > 0.3:
-            raise ValidationError(u"Sum of max value of all features should be less then or equal to 30%")
+            raise ValidationError(u"Sum of max value of all features for lot should be less then or equal to 25%")
+        elif features and not data['lots'] and round(vnmax(features), 15) > 0.25:
+            raise ValidationError(u"Sum of max value of all features should be less then or equal to 25%")
 
     def validate_auctionUrl(self, data, url):
         if url and data['lots']:
