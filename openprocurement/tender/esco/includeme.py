2d89c32bfe540db542ef18927b791d7812e12a98
annawzz <annawzz@quintagroup.org>
2017-09-28 16:54:22 +0300

    Add request property now

--- openprocurement/tender/esco/includeme.py
+++ openprocurement/tender/esco/includeme.py
@@ -3,13 +3,14 @@ from pyramid.interfaces import IRequest
 from openprocurement.api.interfaces import IContentConfigurator
 from openprocurement.tender.esco.models import Tender, IESCOTender
 from openprocurement.tender.esco.adapters import TenderESCOConfigurator
-
+from openprocurement.tender.esco.utils import request_get_now
 
 
 def includeme(config):
     config.add_tender_procurementMethodType(Tender)
     config.scan("openprocurement.tender.esco.views")
     config.scan("openprocurement.tender.esco.subscribers")
+    config.add_request_method(request_get_now, 'now', reify=True)
     config.registry.registerAdapter(TenderESCOConfigurator,
                                     (IESCOTender, IRequest),
                                     IContentConfigurator)
--- openprocurement/tender/esco/utils.py
+++ openprocurement/tender/esco/utils.py
@@ -1,4 +1,5 @@
 from openprocurement.tender.esco.constants import NPV_CALCULATION_DURATION
+from openprocurement.api.utils import get_now
 
 
 def calculate_npv(nbu_rate, annualCostsReduction, yearlyPayments,
@@ -8,3 +9,7 @@ def calculate_npv(nbu_rate, annualCostsReduction, yearlyPayments,
     value = sum([(CR if i <= contractDuration else CRfree)/(1 + nbu_rate)**i
                  for i in range(1, NPV_CALCULATION_DURATION + 1)])
     return round(value, 3)
+
+
+def request_get_now(request):
+    return get_now()
