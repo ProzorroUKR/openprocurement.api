c18aa2530e589f25f4f5617e9b9f4d924d2ebfa4
annawzz <annawzz@quintagroup.org>
2017-10-05 11:35:07 +0300

    Remove adding request method (moved to api)

--- openprocurement/tender/esco/utils.py
+++ openprocurement/tender/esco/utils.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 from decimal import Decimal
-from openprocurement.api.utils import get_now
 
-
-def request_get_now(request):
-    return get_now()
 
 def to_decimal(fraction):
     return Decimal(fraction.numerator) / Decimal(fraction.denominator)
