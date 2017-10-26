7dd5acd376daeeacf96d8783244623686bf810de
annawzz <annawzz@quintagroup.org>
2017-10-26 17:01:31 +0300

    Prepare openprocurement.tender.openeu 2.4.2.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.4.1'
+version = '2.4.2'
 
 requires = [
     'setuptools',
     'openprocurement.api>=2.4',
-    'openprocurement.tender.openua>=2.4.1'
+    'openprocurement.tender.openua>=2.4.2'
 ]
 
 test_requires = requires + [
