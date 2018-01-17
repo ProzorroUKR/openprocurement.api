de6f3f6c88dd6cabe0b63cbc1649fc78d32c632e
Roman Kozlovskyi <kroman0@quintagroup.com>
2018-01-17 15:38:57 +0200

    Skip validation and serialize status for unsuccessful bids

--- openprocurement/tender/openeu/tests/cancellation_blanks.py
+++ openprocurement/tender/openeu/tests/cancellation_blanks.py
@@ -246,6 +246,7 @@ def bids_on_tender_cancellation_in_awarded(self):
 
 def cancellation_active_tendering_j708(self):
     bid = deepcopy(self.initial_bids[0])
+    bid['lotValues'] = bid['lotValues'][:1]
     response = self.app.post_json('/tenders/{}/bids'.format(self.tender_id), {'data': bid})
     self.assertEqual(response.status, '201 Created')
     self.initial_bids_tokens[response.json['data']['id']] = response.json['access']['token']
@@ -278,6 +279,39 @@ def cancellation_active_tendering_j708(self):
     self.app.authorization = ('Basic', ('chronograph', ''))
     response = self.app.patch_json('/tenders/{}'.format(self.tender_id), {"data": {"id": self.tender_id}})
     self.assertEqual(response.json['data']['status'], 'active.pre-qualification')
+
+
+def cancellation_active_qualification_j1427(self):
+    bid = deepcopy(self.initial_bids[0])
+    bid['lotValues'] = bid['lotValues'][:1]
+    response = self.app.post_json('/tenders/{}/bids'.format(self.tender_id), {'data': bid})
+    self.assertEqual(response.status, '201 Created')
+    self.initial_bids_tokens[response.json['data']['id']] = response.json['access']['token']
+    self.initial_bids.append(response.json['data'])
+    bid_id = response.json['data']['id']
+
+    self.set_status('active.pre-qualification', {"id": self.tender_id, 'status': 'active.tendering'})
+    self.app.authorization = ('Basic', ('chronograph', ''))
+    response = self.app.patch_json('/tenders/{}'.format(self.tender_id), {"data": {"id": self.tender_id}})
+    self.assertEqual(response.json['data']['status'], 'active.pre-qualification')
+
+    response = self.app.get('/tenders/{}/qualifications'.format(self.tender_id))
+    qualification_id = [i['id'] for i in response.json['data'] if i['bidID'] == bid_id][0]
+    response = self.app.patch_json('/tenders/{}/qualifications/{}?acc_token={}'.format(self.tender_id, qualification_id, self.tender_token),
+        {"data": {"status": "unsuccessful"}})
+
+    response = self.app.post_json('/tenders/{}/cancellations?acc_token={}'.format(self.tender_id, self.tender_token), {'data': {
+        'reason': 'cancellation reason',
+        'status': 'active',
+        "cancellationOf": "lot",
+        "relatedLot": self.initial_lots[0]['id']
+    }})
+    self.assertEqual(response.status, '201 Created')
+    self.assertEqual(response.content_type, 'application/json')
+
+    response = self.app.get('/tenders/{}/bids/{}'.format(self.tender_id, bid_id))
+    self.assertEqual(response.status, '200 OK')
+    self.assertEqual(response.json['data']['status'], 'unsuccessful')
 
 
 def cancellation_active_qualification(self):
