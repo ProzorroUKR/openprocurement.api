6633f3e2538f7fe86bacf1948b70c4fcf52df952
VDigitall <vitalldh@gmail.com>
2018-03-12 15:21:57 +0200

    Disable require additionalClassification for classification.id == 99999999-9

--- openprocurement/tender/limited/tests/tender_blanks.py
+++ openprocurement/tender/limited/tests/tender_blanks.py
@@ -5,6 +5,9 @@ from uuid import uuid4
 from openprocurement.api.utils import get_now
 from openprocurement.api.constants import ROUTE_PREFIX, CPV_ITEMS_CLASS_FROM
 
+from openprocurement.tender.core.constants import (
+    NOT_REQUIRED_ADDITIONAL_CLASSIFICATION_FROM
+)
 from openprocurement.tender.belowthreshold.tests.base import test_organization
 
 
@@ -422,17 +425,22 @@ def create_tender_invalid(self):
     if get_now() > CPV_ITEMS_CLASS_FROM:
         cpv_code = self.initial_data["items"][0]['classification']['id']
         self.initial_data["items"][0]['classification']['id'] = '99999999-9'
-    response = self.app.post_json(request_path, {'data': self.initial_data}, status=422)
+    status = 422 if get_now() < NOT_REQUIRED_ADDITIONAL_CLASSIFICATION_FROM else 201
+    response = self.app.post_json(request_path, {'data': self.initial_data}, status=status)
     self.initial_data["items"][0]["additionalClassifications"] = data
     if get_now() > CPV_ITEMS_CLASS_FROM:
         self.initial_data["items"][0]['classification']['id'] = cpv_code
-    self.assertEqual(response.status, '422 Unprocessable Entity')
-    self.assertEqual(response.content_type, 'application/json')
-    self.assertEqual(response.json['status'], 'error')
-    self.assertEqual(response.json['errors'], [
-        {u'description': [{u'additionalClassifications': [u'This field is required.']}], u'location': u'body',
-         u'name': u'items'}
-    ])
+    if status == 201:
+        self.assertEqual(response.status, '201 Created')
+        self.assertEqual(response.content_type, 'application/json')
+    else:
+        self.assertEqual(response.status, '422 Unprocessable Entity')
+        self.assertEqual(response.content_type, 'application/json')
+        self.assertEqual(response.json['status'], 'error')
+        self.assertEqual(response.json['errors'], [
+            {u'description': [{u'additionalClassifications': [u'This field is required.']}], u'location': u'body',
+             u'name': u'items'}
+        ])
 
     data = self.initial_data["items"][0]["additionalClassifications"][0]["scheme"]
     self.initial_data["items"][0]["additionalClassifications"][0]["scheme"] = 'Не ДКПП'
@@ -617,6 +625,32 @@ def create_tender(self):
     self.assertNotIn('locality', response.json['data']['items'][0]['deliveryAddress'])
     self.assertNotIn('streetAddress', response.json['data']['items'][0]['deliveryAddress'])
     self.assertNotIn('region', response.json['data']['items'][0]['deliveryAddress'])
+
+    initial_data = deepcopy(self.initial_data)
+    initial_data['items'][0]['classification']['id'] = "99999999-9"
+    additional_classification =\
+        initial_data['items'][0].pop('additionalClassifications')
+    additional_classification[0]['scheme'] = "specialNorms"
+    if get_now() > NOT_REQUIRED_ADDITIONAL_CLASSIFICATION_FROM:
+        response = self.app.post_json('/tenders', {"data": initial_data})
+        self.assertEqual(response.status, '201 Created')
+        self.assertEqual(response.content_type, 'application/json')
+        tender = response.json['data']
+        self.assertEqual(tender['items'][0]['classification']['id'],
+                         '99999999-9')
+        self.assertNotIn('additionalClassifications', tender['items'][0])
+
+    initial_data['items'][0]['additionalClassifications'] =\
+        additional_classification
+    response = self.app.post_json('/tenders', {"data": initial_data})
+    self.assertEqual(response.status, '201 Created')
+    self.assertEqual(response.content_type, 'application/json')
+    tender = response.json['data']
+    self.assertEqual(tender['items'][0]['classification']['id'], '99999999-9')
+    self.assertEqual(
+        tender['items'][0]['additionalClassifications'],
+        additional_classification
+    )
 
 
 def patch_tender(self):
