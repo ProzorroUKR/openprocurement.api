7bb9479a7e1d15dfa97e2cacb93cd0222bbf9bda
VDigitall <vitalldh@gmail.com>
2018-03-12 10:51:26 +0200

    Disable required additionalClassification for classificaton.id == 999999-9

--- openprocurement/tender/core/constants.py
+++ openprocurement/tender/core/constants.py
@@ -9,3 +9,5 @@ AUCTION_STAND_STILL_TIME = timedelta(minutes=15)
 CANT_DELETE_PERIOD_START_DATE_FROM = datetime(2016, 9, 23, tzinfo=TZ)
 COMPLAINT_STAND_STILL_TIME = timedelta(days=3)
 BID_LOTVALUES_VALIDATION_FROM = datetime(2016, 10, 21, tzinfo=TZ)
+# Set non required additionalClassification for classification_id 999999-9
+NOT_REQUIRED_ADDITIONAL_CLASSIFICATION_FROM = datetime(2018, 4, 20, tzinfo=TZ)
