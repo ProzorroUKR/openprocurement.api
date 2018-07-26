c3448be56d31d170ee8d65f6c0317d44c9ce3e07
Yaroslav Shalenyk <yshalenyk@quintagroup.com>
2018-07-26 18:17:25 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1.0'
+
+requires = [
+    'setuptools',
+    'openprocurement.api>=2.4',
+    'openprocurement.agreement.core'
+]
+test_requires = requires + [
+    'webtest',
+    'mock',
+    'pytest',
+    'nose', # TODO: remove from travis
+    'pytest-cov'
+]
+docs_requires = requires + [
+    'sphinxcontrib-httpdomain',
+]
+
+entry_points = {
+    # 'openprocurement.api.plugins': [
+    #     'tender_core = openprocurement.tender.cfaua.includeme:includeme'
+    # ],
+    # 'openprocurement.tender.cfaua.migrations': [
+        # 'contracts = openprocurement.tender.cfaua.migration:migrate_data'
+    # ]
+}
+
+setup(name='openprocurement.agreement.cfaua',
+      version=version,
+      description="",
+      long_description=open("README.md").read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python",
+      ],
+      keywords='',
+      author='Quintagroup, Ltd.',
+      author_email='info@quintagroup.com',
+      # url='https://github.com/openprocurement/openprocurement.agreement.core',
+      license='Apache License 2.0',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['openprocurement', 'openprocurement.agreement'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=test_requires,
+      extras_require={'test': test_requires, 'docs': docs_requires},
+      # test_suite="openprocurement.agreement.cfaua.tests.main.suite",
+      entry_points=entry_points
+      )
