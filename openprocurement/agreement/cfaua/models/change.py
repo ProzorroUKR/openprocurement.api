2dd5098340bb20ccd1d99f5e18943fbe3bcc0e2d
Taras Vaskiv <codertarasvaskiv@quintagroup.org>
2018-09-12 09:58:17 +0300

    Extend from base change

--- openprocurement/agreement/cfaua/models/change.py
+++ openprocurement/agreement/cfaua/models/change.py
@@ -1,31 +1,19 @@
 # -*- coding: utf-8 -*-
-from uuid import uuid4
 
 from openprocurement.api.roles import RolesFromCsv
-from schematics.types import StringType, MD5Type
-from schematics.exceptions import ValidationError
-from openprocurement.api.utils import get_now
-from openprocurement.api.models import Model, ListType, IsoDateTimeType
+from schematics.types import StringType
+from openprocurement.api.models import ListType
+
+from openprocurement.agreement.core.models.change import Change as BaseChange
 
 
-class Change(Model):
+class Change(BaseChange):
     class Options:
         roles = RolesFromCsv('Change.csv', relative_to=__file__)
 
-    id = MD5Type(required=True, default=lambda: uuid4().hex)
-    status = StringType(choices=['pending', 'active'], default='pending')
-    date = IsoDateTimeType(default=get_now)
-    rationale = StringType(required=True, min_length=1)
-    rationale_en = StringType()
-    rationale_ru = StringType()
     rationaleTypes = ListType(StringType(choices=['volumeCuts', 'itemPriceVariation',
                                                   'qualityImprovement', 'thirdParty',
                                                   'durationExtension', 'priceReduction',
                                                   'taxRate', 'fiscalYearExtension'],
                                          required=True), min_size=1, required=True)
     agreementNumber = StringType()
-    dateSigned = IsoDateTimeType()
-
-    def validate_dateSigned(self, data, value):
-        if value and value > get_now():
-            raise ValidationError(u"Agreement signature date can't be in the future")
