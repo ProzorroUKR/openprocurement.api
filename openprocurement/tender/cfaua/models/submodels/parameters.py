9f389dc81c9bd158444b3d2a2888362ba7694851
andrii-shchudlo <andriis@quintagroup.org>
2018-10-11 15:33:53 +0300

    Refactoring

--- openprocurement/tender/cfaua/models/submodels/parameters.py
+++ openprocurement/tender/cfaua/models/submodels/parameters.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
-from decimal import Decimal
-from openprocurement.api.models import DecimalType, Model
+from openprocurement.api.models import DecimalType
 from openprocurement.api.roles import RolesFromCsv
 from openprocurement.tender.core.models import Parameter as BaseParameter, bids_validation_wrapper, get_tender
-from schematics.exceptions import ValidationError
-
 
 class Parameter(BaseParameter):
     class Options:
@@ -12,15 +9,14 @@ class Parameter(BaseParameter):
 
     value = DecimalType(required=True, precision=-2)
 
+
+class BidParameter(Parameter):
+
     @bids_validation_wrapper
     def validate_value(self, data, value):
-        if isinstance(data['__parent__'], Model):
-            tender = get_tender(data['__parent__'])
-            codes = dict([(i.code, [x.value for x in i.enum]) for i in (tender.features or [])])
-            if data['code'] in codes and Decimal(str(value)) not in codes[data['code']]:
-                raise ValidationError(u"value should be one of feature value.")
+        Parameter._validator_functions['value'](self, data, value)
 
     @bids_validation_wrapper
     def validate_code(self, data, code):
-        BaseParameter._validator_functions['code'](self, data, code)
+        Parameter._validator_functions['code'](self, data, code)
 
