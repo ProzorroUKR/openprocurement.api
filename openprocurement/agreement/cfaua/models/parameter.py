5848c42b87abafbf501362766a1449ddf93f0cd1
andrii-shchudlo <andriis@quintagroup.org>
2018-10-12 16:14:15 +0300

    Change parameter and feature in decimal type

--- openprocurement/agreement/cfaua/models/parameter.py
+++ openprocurement/agreement/cfaua/models/parameter.py
@@ -1,7 +1,7 @@
+from decimal import Decimal
 from schematics.exceptions import ValidationError
-from schematics.types import StringType, FloatType
-
-from openprocurement.api.models import Model
+from schematics.types import StringType
+from openprocurement.api.models import Model, DecimalType
 from openprocurement.api.roles import RolesFromCsv
 from openprocurement.agreement.core.utils import get_agreement
 
@@ -11,7 +11,7 @@ class Parameter(Model):
         serialize_when_none = False
         roles = RolesFromCsv('Parameter.csv', relative_to=__file__)
     code = StringType(required=True)
-    value = FloatType(required=True)
+    value = DecimalType(required=True)
 
     def validate_code(self, data, code):
         if isinstance(data['__parent__'], Model) and \
@@ -22,5 +22,5 @@ class Parameter(Model):
         if isinstance(data['__parent__'], Model):
             agreement = get_agreement(data['__parent__'])
             codes = dict([(i.code, [x.value for x in i.enum]) for i in (agreement.features or [])])
-            if data['code'] in codes and value not in codes[data['code']]:
+            if data['code'] in codes and Decimal(str(value)) not in codes[data['code']]:
                 raise ValidationError(u"value should be one of feature value.")
