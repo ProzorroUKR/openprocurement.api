d9f1922bb46150c3cff3aca53ba9f59d62741b50
smithumble <smithumble@gmail.com>
2019-10-16 09:14:47 +0300

    Clean imports in core base tests

--- src/openprocurement/agreement/core/tests/base.py
+++ src/openprocurement/agreement/core/tests/base.py
@@ -1,9 +1,6 @@
 import json
-import uuid
 import os.path
-from copy import deepcopy
-from openprocurement.api.utils import get_now
-from openprocurement.tender.core.tests.base import BaseWebTest, change_auth
+from openprocurement.tender.core.tests.base import BaseWebTest
 
 here = os.path.dirname(os.path.abspath(__file__))
 
