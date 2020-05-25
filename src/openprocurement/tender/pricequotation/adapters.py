ce657da4162e7256c1615f336703c109b010ac13
Yaroslav Shalenyk <yshalenyk@gmail.com>
2020-05-25 15:19:51 +0300

    Fix description of pricequotation configurator

--- src/openprocurement/tender/pricequotation/adapters.py
+++ src/openprocurement/tender/pricequotation/adapters.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 from openprocurement.tender.core.adapters import TenderConfigurator
-from openprocurement.tender.openua.constants import STATUS4ROLE
 from openprocurement.tender.pricequotation.models import PriceQuotationTender
 
 
 class PQTenderConfigurator(TenderConfigurator):
-    """ Reporting Tender configuration adapter """
+    """ Price Quotation Tender configuration adapter """
 
-    name = "Reporting Tender configurator"
+    name = "Price Quotation Tender configurator"
     model = PriceQuotationTender
