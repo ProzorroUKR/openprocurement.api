e4caf71ec96eef4e843b7936114c7a8b8513773a
sinusx <aleksey.mikhaylovskiy@gmail.com>
2020-05-28 17:31:24 +0300

    shift mock datetime

--- docs/tests/base/constants.py
+++ docs/tests/base/constants.py
@@ -6,4 +6,4 @@ DOCS_URL = 'http://{}'.format(DOCS_HOST)
 AUCTIONS_HOST = 'auction-sandbox.prozorro.gov.ua'
 AUCTIONS_URL = 'http://{}'.format(AUCTIONS_HOST)
 
-MOCK_DATETIME = '2020-05-01T00:00:00+02:00'
+MOCK_DATETIME = '2020-05-15T00:00:00+02:00'
