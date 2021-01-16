5dde5a5ad0ff09940004ff6ee1fe05f966605853
smithumble <smithumble@gmail.com>
2021-01-16 18:57:37 +0200

    Add tender document bulk test

--- src/openprocurement/tender/belowthreshold/tests/document.py
+++ src/openprocurement/tender/belowthreshold/tests/document.py
@@ -15,6 +15,7 @@ from openprocurement.tender.belowthreshold.tests.document_blanks import (
     create_tender_document_error,
     create_tender_document_json_invalid,
     create_tender_document_json,
+    create_tender_document_json_bulk,
     put_tender_document_json,
     # TenderLotDocumentWithDSResourceTest
     lot_patch_tender_document_json_lots_none,
@@ -32,6 +33,7 @@ class TenderDocumentResourceTestMixin(object):
 class TenderDocumentWithDSResourceTestMixin(object):
     test_create_tender_document_json_invalid = snitch(create_tender_document_json_invalid)
     test_create_tender_document_json = snitch(create_tender_document_json)
+    test_create_tender_document_json_bulk = snitch(create_tender_document_json_bulk)
     test_put_tender_document_json = snitch(put_tender_document_json)
 
 
