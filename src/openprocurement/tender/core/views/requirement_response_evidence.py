8d139e25c1d9039b64ce919eee4149b6a6245128
smithumble <smithumble@gmail.com>
2021-02-04 11:27:23 +0200

    Fix bid rr dateModified update in tender

--- src/openprocurement/tender/core/views/requirement_response_evidence.py
+++ src/openprocurement/tender/core/views/requirement_response_evidence.py
@@ -9,10 +9,14 @@ from openprocurement.tender.core.utils import save_tender, apply_patch
 
 class BaseRequirementResponseEvidenceResource(APIResource):
 
+    def pre_save(self):
+        pass
+
     def collection_post(self):
         context_name = self.request.context["__parent__"].__class__.__name__.lower()
         evidence = self.request.validated["evidence"]
         self.request.context.evidences.append(evidence)
+        self.pre_save()
         if save_tender(self.request):
             self.LOGGER.info(
                 "Created {} requirement response evidence {}".format(context_name, evidence.id),
@@ -40,6 +44,7 @@ class BaseRequirementResponseEvidenceResource(APIResource):
         context_name = self.request.context["__parent__"]["__parent__"].__class__.__name__.lower()
         apply_patch(self.request, save=False, src=evidence.serialize())
 
+        self.pre_save()
         if save_tender(self.request):
             self.LOGGER.info(
                 "Updated {} requirement response evidence {}".format(context_name, evidence.id),
@@ -56,7 +61,7 @@ class BaseRequirementResponseEvidenceResource(APIResource):
         res = evidence.serialize("view")
 
         self.request.validated["requirement_response"].evidences.remove(evidence)
-        self.request.validated["tender"].modified = False
+        self.pre_save()
         if save_tender(self.request):
             self.LOGGER.info(
                 "Deleted {} evidence {}".format(context_name, self.request.context.id),
