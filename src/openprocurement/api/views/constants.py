4f8f294e9f28279d7b9f30e0a436889f0aeae571
stepan.rasputny <stepan.rasputny@raccoongang.com>
2021-07-08 14:01:06 +0300

    Fix constants path

--- src/openprocurement/api/views/constants.py
+++ src/openprocurement/api/views/constants.py
@@ -2,14 +2,14 @@ from os.path import dirname, join
 import configparser
 from cornice.service import Service
 from pyramid.response import Response
+from openprocurement.api.constants import CONSTANTS_FILE_PATH
 
 constants = Service(name="constants", path="/constants", renderer="json")
 
 
 @constants.get()
 def get_constants(request):
-    file_path = join(dirname(dirname(__file__))) + '/constants.ini'
     config = configparser.ConfigParser()
-    config.read(file_path)
+    config.read(CONSTANTS_FILE_PATH)
     result = {k.upper(): v for k, v in config["DEFAULT"].items()}
     return Response(json_body=result)
