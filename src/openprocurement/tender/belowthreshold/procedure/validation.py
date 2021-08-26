33ef5537d1102daebd0d92b000e4dfefe8506b46
stepan.rasputny <stepan.rasputny@raccoongang.com>
2021-08-26 12:54:33 +0300

    Fix upload docs for belowThreshold

--- src/openprocurement/tender/belowthreshold/procedure/validation.py
+++ src/openprocurement/tender/belowthreshold/procedure/validation.py
@@ -4,6 +4,7 @@ from openprocurement.api.validation import OPERATIONS
 from openprocurement.api.constants import GUARANTEE_ALLOWED_TENDER_TYPES
 from openprocurement.tender.core.procedure.context import get_now
 from openprocurement.tender.core.procedure.documents import check_document, update_document_url
+from json.decoder import JSONDecodeError
 
 
 # BID DOCUMENTS
@@ -54,10 +55,28 @@ def validate_bid_document_operation_with_not_pending_award(request, **kwargs):
 
 
 def validate_upload_documents_not_allowed_for_simple_pmr(request, **kwargs):
+    try:
+        doc_type = request.json["data"].get("documentType", "") == "contractGuarantees"
+    except JSONDecodeError:
+        doc_type = True
+    except AttributeError:
+        docs = request.json["data"]
+        doc_type = all(doc.get("documentType", "") != "contractGuarantees" for doc in docs)
     tender = request.validated["tender"]
     statuses = ("active.qualification",)
     if tender["status"] in statuses and tender.get("procurementMethodRationale") == "simple":
-        raise_operation_error(
-            request,
-            f"Can't upload document with {statuses} tender status and procurementMethodRationale simple"
-        )
+        if tender.get("procurementMethodRationale") == "simple":
+            bid_id = request.validated["bid"]["id"]
+            criteria = tender["criteria"]
+            awards = tender["awards"]
+            bid_with_active_award = any([award["status"] == "active" and award["bid_id"] == bid_id for award in awards])
+            needed_criterion = any(
+                [criterion["classification"]["id"] == "CRITERION.OTHER.CONTRACT.GUARANTEE" for criterion in criteria]
+            )
+            if not all([doc_type, needed_criterion, bid_with_active_award]):
+                raise_operation_error(
+                    request,
+                    "Can't upload document with {} tender status and procurementMethodRationale simple".format(
+                        statuses
+                    ),
+                )
