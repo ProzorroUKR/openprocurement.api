dbda3cbdbd554d732d5aea7882cfa0a449d35df5
Aleksey Stryukov <aleksey.stryukov@gmail.com>
2021-12-14 14:46:50 +0200

    PQ: Disabling all required validation

--- src/openprocurement/tender/pricequotation/procedure/models/bid.py
+++ src/openprocurement/tender/pricequotation/procedure/models/bid.py
@@ -1,10 +1,7 @@
 from schematics.types import StringType, MD5Type
 from schematics.types.compound import ModelType
 from schematics.types.serializable import serializable
-from openprocurement.api.models import Model, Value
-from openprocurement.api.validation import OPERATIONS
-from openprocurement.api.utils import raise_operation_error
-from openprocurement.tender.core.procedure.context import get_tender, get_request
+from openprocurement.tender.core.procedure.context import get_tender, get_request, get_now
 from openprocurement.tender.core.procedure.models.bid import get_default_bid_status
 from openprocurement.tender.core.procedure.models.document import PostDocument, Document
 from openprocurement.tender.core.procedure.models.base import (
@@ -12,10 +9,90 @@ from openprocurement.tender.core.procedure.models.base import (
     PatchBusinessOrganization,
     PostBusinessOrganization,
 )
+from schematics.exceptions import ValidationError
+from openprocurement.api.models import Model, Value
+from openprocurement.api.utils import raise_operation_error, get_first_revision_date
+from openprocurement.api.constants import PQ_CRITERIA_RESPONSES_ALL_FROM
+from openprocurement.api.validation import OPERATIONS
+from openprocurement.tender.core.validation import TYPEMAP
 from openprocurement.tender.pricequotation.procedure.models.req_response import RequirementResponse
-from openprocurement.tender.pricequotation.validation import _validate_requirement_responses
 from openprocurement.tender.pricequotation.procedure.validation import validate_bid_value
 from uuid import uuid4
+
+
+def validate_requirement_responses(criterias, req_responses):
+    requirements = {r["id"]: r
+                    for c in criterias
+                    for g in c.get("requirementGroups", "")
+                    for r in g.get("requirements", "")}
+    expected_ids = set(requirements.keys())
+    actual_ids = {r["requirement"]["id"] for r in req_responses}
+    if len(actual_ids) != len(req_responses):
+        raise ValidationError(f'Duplicate references for criterias')
+
+    diff = expected_ids - actual_ids
+    if diff and get_first_revision_date(get_tender(), default=get_now()) > PQ_CRITERIA_RESPONSES_ALL_FROM:
+        raise ValidationError(f'Missing references for criterias: {list(diff)}')
+
+    additional = actual_ids - expected_ids
+    if additional:
+        raise ValidationError(f'No such criteria with id {additional}')
+
+    for response in req_responses:
+        response_id = response["requirement"]["id"]
+        _matches(requirements[response_id], response)
+
+
+def _matches(criteria, response):
+    datatype = TYPEMAP[criteria['dataType']]
+    # validate value
+    value = datatype.to_native(response['value'])
+
+    expected = criteria.get('expectedValue')
+    min_value = criteria.get('minValue')
+    max_value = criteria.get('maxValue')
+
+    if expected:
+        expected = datatype.to_native(expected)
+        if datatype.to_native(expected) != value:
+            raise ValidationError(
+                'Value "{}" does not match expected value "{}" in reqirement {}'.format(
+                    value, expected, criteria['id']
+                )
+            )
+    if min_value and max_value:
+        min_value = datatype.to_native(min_value)
+        max_value = datatype.to_native(max_value)
+        if value < min_value or value > max_value:
+            raise ValidationError(
+                'Value "{}" does not match range from "{}" to "{}" in reqirement {}'.format(
+                    value,
+                    min_value,
+                    max_value,
+                    criteria['id']
+                )
+            )
+
+    if min_value and not max_value:
+        min_value = datatype.to_native(min_value)
+        if value < min_value:
+            raise ValidationError(
+                'Value {} is lower then minimal required {} in reqirement {}'.format(
+                    value,
+                    min_value,
+                    criteria['id']
+                )
+            )
+    if not min_value and max_value:
+        if value > datatype.to_native(max_value):
+            raise ValidationError(
+                'Value {} is higher then required {} in reqirement {}'.format(
+                    value,
+                    max_value,
+                    criteria['id']
+                )
+            )
+    return response
 
 
 class PatchBid(Model):
@@ -31,10 +108,10 @@ class PatchBid(Model):
     def validate_tenderers(self, _, value):
         if value and value[0].identifier:
             tenderer_id = value[0].identifier.id
-            if tenderer_id and tenderer_id not in (
+            if tenderer_id and tenderer_id not in {
                 i["identifier"]["id"]
                 for i in get_tender().get("shortlistedFirms", "")
-            ):
+            }:
                 # it's 403, not 422, so we can't raise ValueError or ValidationError
                 request = get_request()
                 raise_operation_error(request,
@@ -68,7 +145,7 @@ class PostBid(PatchBid):
     def validate_requirementResponses(self, data, value):
         tender = get_tender()
         criterion = tender.get("criteria", [])
-        _validate_requirement_responses(criterion, value)
+        validate_requirement_responses(criterion, value)
 
 
 class Bid(Model):
@@ -92,3 +169,8 @@ class Bid(Model):
         required=True,
         min_size=1,
     )
+
+    def validate_requirementResponses(self, data, value):
+        tender = get_tender()
+        criterion = tender.get("criteria", [])
+        validate_requirement_responses(criterion, value)
