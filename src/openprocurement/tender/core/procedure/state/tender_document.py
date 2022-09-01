efbffaaed9706d71561ceaa46da4006c93c18644
Oleksii <aleksey.stryukov@raccoongang.com>
2022-09-01 10:37:36 +0300

    rm tender doc blocked bt cancellation

--- src/openprocurement/tender/core/procedure/state/tender_document.py
+++ src/openprocurement/tender/core/procedure/state/tender_document.py
@@ -5,10 +5,8 @@ from openprocurement.tender.core.procedure.context import get_tender
 
 class TenderDocumentState(BaseDocumentState):
     def validate_document_post(self, data):
-        request, tender = get_request(), get_tender()
-        self.validate_cancellation_blocks(request, tender)
+        pass
 
     def validate_document_patch(self, before, after):
-        request, tender = get_request(), get_tender()
-        self.validate_cancellation_blocks(request, tender)
+        pass
 
