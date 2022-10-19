8897c414b6222ad03cbf54a4e01c5417a2470277
smithumble <smithumble@gmail.com>
2022-10-20 01:28:46 +0300

    Fix cfaua agreements documents urls

--- src/openprocurement/tender/core/procedure/serializers/agreement.py
+++ src/openprocurement/tender/core/procedure/serializers/agreement.py
@@ -1,10 +1,8 @@
 from openprocurement.tender.core.procedure.serializers.base import BaseSerializer, ListSerializer
 from openprocurement.tender.core.procedure.serializers.document import ConfidentialDocumentSerializer
-from openprocurement.tender.core.procedure.serializers.complaint import ComplaintSerializer
 
 
 class AgreementSerializer(BaseSerializer):
     serializers = {
         "documents": ListSerializer(ConfidentialDocumentSerializer),
-        # "complaints": ListSerializer(ComplaintSerializer),
     }
