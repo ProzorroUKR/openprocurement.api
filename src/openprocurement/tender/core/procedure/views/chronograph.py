73b3c40ff1c9f80c168b87017f54b35e14f99b4b
smithumble <smithumble@gmail.com>
2023-04-25 01:45:13 +0300

    Use context for tender config in chronograph and auction views

--- src/openprocurement/tender/core/procedure/views/chronograph.py
+++ src/openprocurement/tender/core/procedure/views/chronograph.py
@@ -1,4 +1,8 @@
 from openprocurement.api.utils import json_view, context_unpack
+from openprocurement.tender.core.procedure.context import (
+    get_tender,
+    get_tender_config,
+)
 from openprocurement.tender.core.procedure.views.base import TenderBaseResource
 from openprocurement.tender.core.procedure.validation import validate_input_data
 from openprocurement.tender.core.procedure.utils import save_tender, apply_data_patch
@@ -11,11 +15,9 @@ class TenderChronographResource(TenderBaseResource):
 
     @json_view(permission="chronograph")
     def get(self):
-        tender = self.request.validated["tender"]
-        tender_config = self.request.validated["tender_config"]
         return {
-            "data": self.serializer_class(tender).data,
-            "config": self.serializer_config_class(tender_config).data,
+            "data": self.serializer_class(get_tender()).data,
+            "config": get_tender_config(),
         }
 
     @json_view(
@@ -25,8 +27,6 @@ class TenderChronographResource(TenderBaseResource):
         )
     )
     def patch(self):
-        tender_config = self.request.validated["tender_config"]
-
         # 1 we convert [{"auctionPeriod": {"startDate": "2020.."}}, {"auctionPeriod": None}]
         #           to [{"auctionPeriod": {"startDate": "2020.."}}, {}]
         # TODO find a better way to specify partial update
@@ -56,6 +56,6 @@ class TenderChronographResource(TenderBaseResource):
                 extra=context_unpack(self.request, {"MESSAGE_ID": "tender_chronograph_patch"})
             )
         return {
-            "data": self.serializer_class(self.request.validated["tender"]).data,
-            "config": self.serializer_config_class(tender_config).data,
+            "data": self.serializer_class(get_tender()).data,
+            "config": get_tender_config(),
         }
