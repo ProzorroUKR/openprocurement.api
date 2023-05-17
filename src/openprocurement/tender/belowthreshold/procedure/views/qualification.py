e2943db35bd2aacad29a07af6ed72fc6d1eacf5f
smithumble <smithumble@gmail.com>
2023-05-17 08:27:17 +0300

    Add qualification views to below

---
+++ src/openprocurement/tender/belowthreshold/procedure/views/qualification.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from openprocurement.tender.core.procedure.views.qualification import TenderQualificationResource
+from cornice.resource import resource
+
+
+@resource(
+    name="belowThreshold:Tender Qualification",
+    collection_path="/tenders/{tender_id}/qualifications",
+    path="/tenders/{tender_id}/qualifications/{qualification_id}",
+    procurementMethodType="belowThreshold",
+    description="belowThreshold Qualification",
+)
+class TenderEUQualificationResource(TenderQualificationResource):
+    pass
