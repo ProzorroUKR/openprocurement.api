c506cbe7b9f4c7674f274f22a848e02a5ea52835
smithumble <smithumble@gmail.com>
2023-07-10 17:18:46 +0300

    Fix qualified and eligible defaults

--- src/openprocurement/tender/core/procedure/models/qualification.py
+++ src/openprocurement/tender/core/procedure/models/qualification.py
@@ -1,3 +1,5 @@
+from schematics.types.serializable import serializable
+
 from openprocurement.api.models import IsoDateTimeType, ValidationError
 from openprocurement.tender.core.procedure.context import get_tender
 from schematics.types import StringType, MD5Type, BooleanType, BaseType
@@ -33,27 +35,45 @@ class Qualification(ObjResponseMixin, PatchQualification, QualificationMilestone
     status = StringType(choices=["pending", "active", "unsuccessful", "cancelled"], default="pending")
     date = IsoDateTimeType()
     documents = ListType(ModelType(Document, required=True))
-    qualified = BooleanType(default=False)  # відсутність підстав для відмови в участі
-    eligible = BooleanType(default=False)  # підтвердити відповідність кваліфікаційним критеріям (17 стаття)
+    qualified = BooleanType()  # відсутність підстав для відмови в участі
+    eligible = BooleanType()  # підтвердити відповідність кваліфікаційним критеріям (17 стаття)
     complaints = BaseType()
 
+    @staticmethod
+    def should_be_qualified():
+        # TODO: find a way to determine, not based on procurementMethodType
+        return get_tender()["procurementMethodType"] != "belowThreshold"
+
+    @staticmethod
+    def should_be_eligible():
+        # TODO: find a way to determine, not based on procurementMethodType
+        return get_tender()["procurementMethodType"] != "belowThreshold"
+
+    @serializable(serialized_name="qualified", serialize_when_none=False)
+    def default_qualified(self):
+        if self.qualified is None and self.should_be_qualified():
+            return False
+        return self.qualified
+
+    @serializable(serialized_name="eligible", serialize_when_none=False)
+    def default_eligible(self):
+        if self.eligible is None and self.should_be_eligible():
+            return False
+        return self.eligible
+
     def validate_qualified(self, data, qualified):
-        tender = get_tender()
-        if tender["procurementMethodType"] == "belowThreshold":
-            # TODO: find a way to skip validation not based on procurementMethodType
+        if not self.should_be_qualified():
             return
         status = data.get("status")
-        if status == "active" and not qualified:
-            raise ValidationError("This field is required.")
+        if status == "active" and qualified is not True:
+            raise ValidationError("This field must be true.")
 
     def validate_eligible(self, data, eligible):
-        tender = get_tender()
-        if tender["procurementMethodType"] == "belowThreshold":
-            # TODO: find a way to skip validation not based on procurementMethodType
+        if not self.should_be_eligible():
             return
         status = data.get("status")
-        if status == "active" and not eligible:
-            raise ValidationError("This field is required.")
+        if status == "active" and eligible is not True:
+            raise ValidationError("This field must be true.")
 
     def validate_lotID(self, data, value):
         lots = get_tender().get("lots")
