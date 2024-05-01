979e6f5798e66586bafce067a2f17af9e6eae9ff
Serhij Andreev <serhij.andreev@raccoongang.com>
2024-05-01 13:25:10 +0000

    Fix contract migration on updating contract remove rogue data from suppliers

--- src/openprocurement/contracting/core/migrations/migrate_contracts.py
+++ src/openprocurement/contracting/core/migrations/migrate_contracts.py
@@ -3,6 +3,7 @@
 from collections import defaultdict
 
 from openprocurement.api.context import set_now
+from openprocurement.api.database import get_public_modified
 from openprocurement.api.procedure.utils import append_revision, get_revision_changes
 
 if __name__ == "__main__":
@@ -23,10 +24,10 @@ logging.basicConfig(level=logging.INFO, format='%(message)s')
 logger = logging.getLogger(__name__)
 
 
-def delete_buyers_attr(objs):
+def delete_buyers_attrs(objs):
     for obj in objs:
-        for attr in ("kind", "scale", "contactPoint", "id"):
-            obj.pop(attr, None)
+        obj.pop("id", None)
+        obj.pop("contactPoint", None)
 
 
 def get_buyer(tender, contract):
@@ -37,29 +38,24 @@ def get_buyer(tender, contract):
                 buyer = i
                 break
 
-    delete_buyers_attr([buyer])
+    delete_buyers_attrs([buyer])
     return buyer
 
 
+def get_bid_credentials(tender, award_id):
+    bid_id = next((a.get("bid_id", "") for a in tender.get("awards", []) if a.get("id") == award_id), "")
+    bid = next((i for i in tender.get("bids", []) if i.get("id", "") == bid_id), tender)
+
+    return bid["owner"], bid["owner_token"]
+
+
 def create_contract(env, tender, tender_contract):
-    if tender_contract.get("suppliers"):
-        delete_buyers_attr(tender_contract["suppliers"])
     if tender.get("mode"):
         tender_contract["mode"] = tender["mode"]
     if tender.get("config"):
         tender_contract["config"] = {"restricted": tender["config"]["restricted"]}
 
-    bid = next(
-        (
-            i
-            for i in tender.get("bids", [])
-            if i.get("id")
-            == next((a.get("bid_id") for a in tender.get("awards", []) if a.get("id") == a.get("awardID")), None)
-        ),
-        tender,
-    )
-
-    tender_contract.pop("date", None)
+    bid_owner, bid_token = get_bid_credentials(tender, tender_contract.get("awardID"))
 
     tender_contract.update(
         {
@@ -67,8 +63,8 @@ def create_contract(env, tender, tender_contract):
             "tender_id": tender["_id"],
             "owner": tender["owner"],
             "tender_token": tender["owner_token"],
-            "bid_owner": bid["owner"],
-            "bid_token": bid["owner_token"],
+            "bid_owner": bid_owner,
+            "bid_token": bid_token,
         }
     )
 
@@ -88,7 +84,17 @@ def create_contract(env, tender, tender_contract):
 
 def update_contract_pe_to_buyer(env, tender, tender_contract, contracting_contract):
     collection = env["registry"].mongodb.contracts.collection
+
     updated_data = {}
+
+    if "bid_owner" not in contracting_contract or "bid_token" not in contracting_contract:
+        bid_owner, bid_token = get_bid_credentials(tender, tender_contract.get("awardID"))
+        updated_data["bid_owner"] = bid_owner
+        updated_data["bid_token"] = bid_token
+
+    if "suppliers" in contracting_contract:
+        delete_buyers_attrs(contracting_contract["suppliers"])
+        updated_data["suppliers"] = contracting_contract["suppliers"]
 
     if tender.get("mode") and not contracting_contract.get("mode"):
         updated_data["mode"] = tender["mode"]
@@ -124,13 +130,15 @@ def run(env, args):
     contracts_collection = env["registry"].mongodb.contracts.collection
     tenders_collection = env["registry"].mongodb.tenders.collection
 
+    contract_statuses = args.s.split(",")
+
     logger.info("Global contracts migration.")
 
     log_every = 100000
     counter = defaultdict(total_contracts=0, updated_contracts=0, created_contracts=0, skipped_contracts=0)
 
     cursor = tenders_collection.find(
-        {"contracts": {"$exists": True}, "procurementMethodType": {"$ne": "esco"}},
+        {"contracts.status": {"$in": contract_statuses}, "procurementMethodType": {"$ne": "esco"}},
         {
             "contracts": 1,
             "buyers": 1,
@@ -153,11 +161,15 @@ def run(env, args):
             contracting_contracts = {
                 i["_id"]: i
                 for i in contracts_collection.find(
-                    {"_id": {"$in": contracts_ids}}, {"procuringEntity": 1, "buyerID": 1}
+                    {"_id": {"$in": contracts_ids}},
+                    {"procuringEntity": 1, "buyerID": 1, "bid_owner": 1, "bid_token": 1, "suppliers": 1, "mode": 1},
                 )
             }
 
             for contract_number, contract in enumerate(tender.get("contracts", "")):
+                if contract.get("status", "") not in contract_statuses:
+                    continue
+
                 contracting_contract = contracting_contracts.get(contract["id"])
                 set_now()
                 if not contracting_contract:
@@ -195,6 +207,15 @@ if __name__ == "__main__":
         default=1000,
         help=(
             "Limits the number of documents returned in one batch. Each batch " "requires a round trip to the server."
+        ),
+    )
+
+    parser.add_argument(
+        "-s",
+        default="pending",
+        help=(
+            "Contract statuses in which contract should be migrated. "
+            "For few statuses separate it with ','. F.E: pending,active"
         ),
     )
     args = parser.parse_args()
