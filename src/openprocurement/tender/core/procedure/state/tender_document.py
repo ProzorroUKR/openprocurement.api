e81b63f6befd3d64dc4346f75793aea717665bed
Serhij Andreev <serhij.andreev@raccoongang.com>
2024-06-19 16:19:47 +0000

    [CS-16946] Allow post and patch `notice` documents with unanswered review request

--- src/openprocurement/tender/core/procedure/state/tender_document.py
+++ src/openprocurement/tender/core/procedure/state/tender_document.py
@@ -2,8 +2,7 @@ from openprocurement.tender.core.procedure.state.document import BaseDocumentSta
 
 
 class TenderDocumentState(BaseDocumentState):
-    def validate_document_post(self, data):
-        self.validate_action_with_exist_inspector_review_request()
-
-    def validate_document_patch(self, before, after):
-        self.validate_action_with_exist_inspector_review_request()
+    def document_always(self, data: dict) -> None:
+        if data.get("documentType") != "notice":
+            self.validate_action_with_exist_inspector_review_request()
+        super().document_always(data)
