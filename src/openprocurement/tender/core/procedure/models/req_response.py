fd0e3012eb5c1aed10940c2082039873154a93e7
Serhij Andreev <serhij.andreev@raccoongang.com>
2024-07-19 08:57:52 +0000

    [CS-17185]Fix skip check requirement responses for criteria with items than not related to bid

--- src/openprocurement/tender/core/procedure/models/req_response.py
+++ src/openprocurement/tender/core/procedure/models/req_response.py
@@ -421,9 +421,15 @@ class PostBidResponsesMixin(ObjResponseMixin):
         for criteria in tender.get("criteria", []):
 
             # Skip criteria for not existing lots (probably)
-            if criteria.get("relatesTo") == "lot":
+            if criteria.get("relatesTo") in ("lot", "item"):
+                if criteria.get("relatesTo") == "item":
+                    item = [item for item in tender.get("items", "") if item["id"] == criteria["relatedItem"]][0]
+                    related_lot = item["relatedLot"]
+                else:
+                    related_lot = criteria["relatedItem"]
+
                 for lotVal in data["lotValues"]:
-                    if criteria["relatedItem"] == lotVal["relatedLot"]:
+                    if related_lot == lotVal["relatedLot"]:
                         break
                 else:
                     continue
