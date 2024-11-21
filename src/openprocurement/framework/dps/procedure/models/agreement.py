ed04017c0ff255c32637acb2a475c77bae438372
smithumble <smithumble@gmail.com>
2024-11-21 04:12:38 +0200

    Remove unused agreement post model

--- src/openprocurement/framework/dps/procedure/models/agreement.py
+++ src/openprocurement/framework/dps/procedure/models/agreement.py
@@ -4,18 +4,10 @@ from openprocurement.api.procedure.types import ModelType
 from openprocurement.framework.core.procedure.models.agreement import (
     Agreement as BaseAgreement,
 )
-from openprocurement.framework.core.procedure.models.agreement import (
-    PostAgreement as BasePostAgreement,
-)
 from openprocurement.framework.core.procedure.models.organization import ProcuringEntity
 from openprocurement.framework.dps.constants import DPS_TYPE
 
 
 class Agreement(BaseAgreement):
-    agreementType = StringType(default=DPS_TYPE)
-    procuringEntity = ModelType(ProcuringEntity, required=True)
-
-
-class PostAgreement(BasePostAgreement):
     agreementType = StringType(default=DPS_TYPE)
     procuringEntity = ModelType(ProcuringEntity, required=True)
