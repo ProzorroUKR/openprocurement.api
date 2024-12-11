55e79dc3aab5e0dc4cc415b6ae0111b69f7fcada
Olha Zamirets <ozamirets@gmail.com>
2024-12-11 18:37:58 +0200

    Remove __parent__ code from core api

--- src/openprocurement/api/procedure/models/base.py
+++ src/openprocurement/api/procedure/models/base.py
@@ -3,18 +3,12 @@ from schematics.transforms import blacklist, convert, export_loop
 from schematics.types import BaseType, StringType
 from schematics.types.serializable import serializable
 
-from openprocurement.api.utils import set_parent
-
 
 class Model(SchematicsModel):
     class Options:
         """Export options for Document."""
 
         serialize_when_none = False
-        roles = {
-            "default": blacklist("__parent__"),
-            "embedded": blacklist("__parent__"),
-        }
 
     __parent__ = BaseType()
 
@@ -23,28 +17,6 @@ class Model(SchematicsModel):
             return getattr(self, name)
         except AttributeError as e:
             raise KeyError(e)
-
-    def __eq__(self, other):
-        if isinstance(other, self.__class__):
-            for k in self._fields:
-                if k != "__parent__" and self.get(k) != other.get(k):
-                    return False
-            return True
-        return NotImplemented
-
-    def convert(self, raw_data, **kw):
-        """
-        Converts the raw data into richer Python constructs according to the
-        fields on the model
-        """
-        value = convert(self.__class__, raw_data, **kw)
-        for i, j in value.items():
-            if isinstance(j, list):
-                for x in j:
-                    set_parent(x, self)
-            else:
-                set_parent(j, self)
-        return value
 
     def to_patch(self, role=None):
         """
@@ -64,17 +36,6 @@ class Model(SchematicsModel):
             print_none=True,
         )
         return data
-
-    def get_role(self):
-        root = self.get_root()
-        request = root.request
-        return "Administrator" if request.authenticated_role == "Administrator" else "edit"
-
-    def get_root(self):
-        root = self.__parent__
-        while root.__parent__ is not None:
-            root = root.__parent__
-        return root
 
 
 class RootModel(Model):
