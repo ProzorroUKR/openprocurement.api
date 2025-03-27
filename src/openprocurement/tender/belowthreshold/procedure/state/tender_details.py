5768e6de696ef661f3cad8bda2b4c4274022a81c
Olha Zamirets <ozamirets@gmail.com>
2025-03-27 14:13:27 +0200

    [CS-19059] Allow active.tendering patch for BELOWTHRESHOLD_FUNDERS_IDS tenders

--- src/openprocurement/tender/belowthreshold/procedure/state/tender_details.py
+++ src/openprocurement/tender/belowthreshold/procedure/state/tender_details.py
@@ -1,4 +1,6 @@
 from openprocurement.api.auth import ACCR_1, ACCR_2, ACCR_5
+from openprocurement.api.context import get_now
+from openprocurement.tender.belowthreshold.constants import TENDERING_EXTRA_PERIOD
 from openprocurement.tender.belowthreshold.procedure.models.tender import (
     PatchDraftTender,
     PatchTender,
@@ -6,8 +8,12 @@ from openprocurement.tender.belowthreshold.procedure.models.tender import (
 from openprocurement.tender.belowthreshold.procedure.state.tender import (
     BelowThresholdTenderState,
 )
+from openprocurement.tender.belowthreshold.procedure.validation import tender_for_funder
 from openprocurement.tender.core.procedure.state.tender_details import (
     TenderDetailsMixing,
+)
+from openprocurement.tender.requestforproposal.procedure.models.tender import (
+    PatchActiveTender,
 )
 
 
@@ -16,6 +22,8 @@ class BelowThresholdTenderDetailsMixing(TenderDetailsMixing):
     tender_central_accreditations = (ACCR_5,)
     tender_edit_accreditations = (ACCR_2,)
 
+    tendering_period_extra_working_days = True
+    tendering_period_extra = TENDERING_EXTRA_PERIOD
     should_validate_notice_doc_required = True
     enquiry_before_tendering = True
     contract_template_required = True
@@ -23,19 +31,24 @@ class BelowThresholdTenderDetailsMixing(TenderDetailsMixing):
 
     def get_patch_data_model(self):
         tender = self.request.validated["tender"]
-        if tender.get("status", "") in ("draft", "active.enquiries"):
+        if tender.get("status", "") == "active.tendering":
+            return PatchActiveTender
+        elif tender.get("status", "") in ("draft", "active.enquiries"):
             return PatchDraftTender
         return PatchTender
 
     def validate_tender_period_extension(self, tender):
-        pass
+        if tender_for_funder(tender):
+            super().validate_tender_period_extension(tender)
 
     @staticmethod
     def set_enquiry_period_invalidation_date(tender):
-        pass
+        if tender_for_funder(tender):
+            tender["enquiryPeriod"]["invalidationDate"] = get_now().isoformat()
 
     def invalidate_bids_data(self, tender):
-        pass
+        if tender_for_funder(tender):
+            super().invalidate_bids_data(tender)
 
 
 class BelowThresholdTenderDetailsState(BelowThresholdTenderDetailsMixing, BelowThresholdTenderState):
--- src/openprocurement/tender/belowthreshold/procedure/validation.py
+++ src/openprocurement/tender/belowthreshold/procedure/validation.py
@@ -1,12 +1,38 @@
+from openprocurement.api.constants_env import BELOWTHRESHOLD_FUNDERS_IDS
 from openprocurement.tender.core.procedure.validation import (
     validate_document_operation_in_allowed_tender_statuses,
     validate_item_operation_in_disallowed_tender_statuses,
+    validate_tender_status_allows_update,
 )
 
+
 # tender
-validate_tender_document_operation_in_allowed_tender_statuses = validate_document_operation_in_allowed_tender_statuses(
-    ("draft", "active.enquiries")
-)
+def tender_for_funder(tender):
+    return tender.get("_id") in BELOWTHRESHOLD_FUNDERS_IDS
+
+
+def validate_tender_status_allows_update_operation(request, **_):
+    allowed_statuses = [
+        "draft",
+        "active.enquiries",
+        "active.pre-qualification",  # state class only allows status change (pre-qualification.stand-still)
+        "active.pre-qualification.stand-still",
+    ]
+
+    if tender_for_funder(request.validated["tender"]):
+        allowed_statuses.append("active.tendering")
+
+    validate_tender_status_allows_update(*allowed_statuses)(request, **_)
+
+
+def validate_tender_document_operation_in_allowed_tender_statuses(request, **_):
+    allowed_statuses = ["draft", "active.enquiries"]
+
+    if tender_for_funder(request.validated["tender"]):
+        allowed_statuses.append("active.tendering")
+
+    validate_document_operation_in_allowed_tender_statuses(allowed_statuses)(request, **_)
+
 
 # lot
 validate_lot_operation_in_disallowed_tender_statuses = validate_item_operation_in_disallowed_tender_statuses(
