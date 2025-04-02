df5fb8d31ec68aae056179a48d0abffa84c5daeb
Olha Zamirets <ozamirets@gmail.com>
2025-04-02 14:10:27 +0300

    [CS-19115] Allow to create/update bids eligible_documents in CFAUA

--- src/openprocurement/tender/core/procedure/views/bid_document.py
+++ src/openprocurement/tender/core/procedure/views/bid_document.py
@@ -22,7 +22,7 @@ from openprocurement.tender.core.procedure.validation import (
     validate_bid_document_operation_in_award_status,
     validate_bid_document_operation_in_bid_status,
     validate_bid_document_operation_period,
-    validate_bid_financial_document_in_tender_status,
+    validate_bid_financial_eligible_document_in_tender_status,
     validate_download_tender_document,
     validate_update_bid_document_confidentiality,
     validate_view_bid_document,
@@ -199,6 +199,61 @@ class BaseTenderBidEligibilityDocumentResource(BaseTenderBidDocumentResource):
 
     container = "eligibilityDocuments"
 
+    @json_view(
+        validators=(
+            validate_item_owner("bid"),
+            validate_post_create_model(allow_bulk=True),
+            unless_allowed_by_qualification_milestone(
+                validate_bid_financial_eligible_document_in_tender_status,
+                validate_bid_document_operation_in_award_status,
+            ),
+            validate_bid_document_operation_period,
+            validate_bid_document_operation_in_bid_status,
+        ),
+        permission="edit_bid",
+    )
+    def collection_post(self):
+        return super().collection_post()
+
+    @json_view(
+        validators=(
+            validate_item_owner("bid"),
+            validate_put_update_model(),
+            unless_allowed_by_qualification_milestone(
+                validate_bid_financial_eligible_document_in_tender_status,
+                validate_bid_document_operation_in_award_status,
+            ),
+            validate_bid_document_operation_period,
+            validate_bid_document_operation_in_bid_status,
+            validate_update_bid_document_confidentiality,
+            update_doc_fields_on_put_document,
+            validate_upload_document,
+            validate_put_model(),
+        ),
+        permission="edit_bid",
+    )
+    def put(self):
+        return super().put()
+
+    @json_view(
+        content_type="application/json",
+        validators=(
+            validate_item_owner("bid"),
+            validate_patch_update_model(none_means_remove=True),
+            validate_patch_model(item_name="document"),
+            unless_allowed_by_qualification_milestone(
+                validate_bid_financial_eligible_document_in_tender_status,
+                validate_bid_document_operation_in_award_status,
+            ),
+            validate_bid_document_operation_period,
+            validate_bid_document_operation_in_bid_status,
+            validate_update_bid_document_confidentiality,
+        ),
+        permission="edit_bid",
+    )
+    def patch(self):
+        return super().patch()
+
 
 @resource(
     name="belowThreshold:Tender Bid Financial Documents",
@@ -238,7 +293,7 @@ class BaseTenderBidFinancialDocumentResource(BaseTenderBidDocumentResource):
             validate_item_owner("bid"),
             validate_input_data(PostDocument, allow_bulk=True),
             unless_allowed_by_qualification_milestone(
-                validate_bid_financial_document_in_tender_status,
+                validate_bid_financial_eligible_document_in_tender_status,
                 validate_bid_document_operation_in_award_status,
             ),
             validate_bid_document_operation_period,
@@ -254,7 +309,7 @@ class BaseTenderBidFinancialDocumentResource(BaseTenderBidDocumentResource):
             validate_item_owner("bid"),
             validate_input_data(PostDocument),
             unless_allowed_by_qualification_milestone(
-                validate_bid_financial_document_in_tender_status,
+                validate_bid_financial_eligible_document_in_tender_status,
                 validate_bid_document_operation_in_award_status,
             ),
             validate_bid_document_operation_period,
@@ -276,7 +331,7 @@ class BaseTenderBidFinancialDocumentResource(BaseTenderBidDocumentResource):
             validate_input_data(PatchDocument, none_means_remove=True),
             validate_patch_data(Document, item_name="document"),
             unless_allowed_by_qualification_milestone(
-                validate_bid_financial_document_in_tender_status,
+                validate_bid_financial_eligible_document_in_tender_status,
                 validate_bid_document_operation_in_award_status,
             ),
             validate_bid_document_operation_period,
