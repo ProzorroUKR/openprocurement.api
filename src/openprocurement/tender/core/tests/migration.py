5486eb2e80e39467c71061dc36daf50b29c14ba8
smithumble <smithumble@gmail.com>
2025-05-19 15:00:43 +0300

    Migrate contractTemplateName format

--- src/openprocurement/tender/core/tests/migration.py
+++ src/openprocurement/tender/core/tests/migration.py
@@ -11,3 +11,7 @@ test_0031_migrate_local_origin_criteria = create_collection_migration_test(
 test_0032_migrate_bids_initial_value = create_collection_migration_test(
     "openprocurement.tender.core.migrations.0032_migrate_bids_initial_value.Migration"
 )
+
+test_0034_migrate_contract_template_name = create_collection_migration_test(
+    "openprocurement.tender.core.migrations.0034_migrate_contract_template_name.Migration"
+)
