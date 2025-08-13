c3cea740ae82840059fc037abc0e8f3d2d259e1b
Olha Zamirets <ozamirets@gmail.com>
2025-08-13 19:06:47 +0300

    Change periodChangesHistory to change (like in contract)

--- src/openprocurement/framework/core/procedure/state/change.py
+++ src/openprocurement/framework/core/procedure/state/change.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from datetime import timedelta
 from logging import getLogger
 
@@ -15,31 +16,49 @@ from openprocurement.tender.core.procedure.utils import dt_from_iso
 LOGGER = getLogger(__name__)
 
 
-class PeriodChangeState(FrameworkState):
+class ChangeState(FrameworkState):
     agreement_class = AgreementState
 
-    def validate_period_change_on_post(self, data, framework):
-        self.validate_period(data, framework)
+    def validate_change_on_post(self, data, framework):
+        self.validate_modifications(data, framework)
+        if "qualificationPeriod" in data.get("modifications", {}):
+            self.validate_period(data["modifications"], framework)
 
     def on_post(self, data, framework):
-        new_period = data.pop("qualificationPeriod", None)
-        data.update(
-            {
-                "prevPeriodEndDate": framework["qualificationPeriod"]["endDate"],
-                "newPeriodEndDate": new_period["endDate"],
-                "date": get_request_now().isoformat(),
-            }
-        )
-        if "periodChangeHistory" not in framework:
-            framework["periodChangeHistory"] = []
-        framework["periodChangeHistory"].append(data)
-        framework["qualificationPeriod"]["endDate"] = new_period["endDate"]
+        if data.get("modifications", {}).get("qualificationPeriod"):
+            new_period_end_date = data["modifications"]["qualificationPeriod"]["endDate"]
+            data["previous"] = {"qualificationPeriod": deepcopy(framework["qualificationPeriod"])}
+            framework["qualificationPeriod"]["endDate"] = new_period_end_date
+        data["date"] = data["dateModified"] = get_request_now().isoformat()
+        if "changes" not in framework:
+            framework["changes"] = []
+        framework["changes"].append(data)
         # recalculate next_check and other objects periods after qualificationPeriod was changed
         self.calculate_framework_periods(framework)
         self.update_agreement(framework)
         self.update_next_check(framework)
         if agreement := self.request.validated.get("agreement"):
             self.agreement_class(self.request, framework=framework).update_next_check(agreement)
+
+    def validate_modifications(self, data, framework):
+        if not data.get("modifications"):
+            raise_operation_error(
+                self.request,
+                "Framework modifications are empty",
+                status=422,
+                name="modifications",
+            )
+        # check if there are any changes
+        updated_framework_data = {}
+        for f, v in data["modifications"].items():
+            if framework.get(f) != v:
+                updated_framework_data[f] = v
+        if not updated_framework_data:
+            raise_operation_error(
+                self.request,
+                "No changes detected between framework and current modifications",
+                status=422,
+            )
 
     def validate_period(self, data, framework):
         if (
@@ -50,7 +69,7 @@ class PeriodChangeState(FrameworkState):
                 get_request(),
                 "Forbidden to change qualification period start date",
                 status=422,
-                name="qualificationPeriod",
+                name="modifications.qualificationPeriod",
             )
         start_date = get_request_now()
         end_date = dt_from_iso(data["qualificationPeriod"]["endDate"])
@@ -66,7 +85,7 @@ class PeriodChangeState(FrameworkState):
                 f"qualificationPeriod.endDate couldn't be less than "
                 f"{SUBMISSION_STAND_STILL_DURATION} full days from now",
                 status=422,
-                name="qualificationPeriod",
+                name="modifications.qualificationPeriod",
             )
 
         end_date_max = calculate_framework_full_date(
@@ -80,5 +99,5 @@ class PeriodChangeState(FrameworkState):
                 get_request(),
                 f"qualificationPeriod.endDate couldn't be more than {MAX_QUALIFICATION_DURATION} full days from now",
                 status=422,
-                name="qualificationPeriod",
+                name="modifications.qualificationPeriod",
             )
