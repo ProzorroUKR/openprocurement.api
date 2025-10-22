164e7ed8f6f33481600163a7d2a8bb3f6affc9d8
Olha Zamirets <ozamirets@gmail.com>
2025-10-22 15:50:49 +0300

    [CS-20470] New field causeDetails for reporting, negotiation

---
+++ src/openprocurement/tender/core/migrations/0036_add_cause_details_for_reporting_negotiation.py
@@ -0,0 +1,316 @@
+import logging
+from copy import deepcopy
+from unittest.mock import ANY
+
+from pymongo import UpdateOne
+
+from openprocurement.api.migrations.base import CollectionMigration, migrate_collection
+from openprocurement.tender.limited.constants import (
+    NEGOTIATION,
+    NEGOTIATION_QUICK,
+    REPORTING,
+)
+from openprocurement.tender.limited.procedure.serializers.tender import (
+    convert_cause_to_cause_details,
+)
+
+logging.basicConfig(level=logging.INFO, format="%(message)s")
+logger = logging.getLogger(__name__)
+
+
+class Migration(CollectionMigration):
+    description = "Migrate tenders complaint posts"
+
+    collection_name = "tenders"
+
+    append_revision = True
+
+    update_date_modified: bool = False
+    update_feed_position: bool = True
+
+    log_every: int = 100000
+    bulk_max_size: int = 500
+
+    def get_filter(self):
+        return {
+            "procurementMethodType": {"$in": [REPORTING, NEGOTIATION, NEGOTIATION_QUICK]},
+            "cause": {"$exists": True},
+            "causeDetails": {"$exists": False},
+        }
+
+    def get_projection(self) -> dict:
+        return {"procurementMethodType": 1, "cause": 1, "causeDescription": 1, "causeDescription_en": 1}
+
+    def update_document(self, doc, context=None):
+        prev_doc = deepcopy(doc)
+        doc = convert_cause_to_cause_details(doc)
+
+        if prev_doc != doc:
+            return doc
+
+        logger.info(f"Tender {doc['_id']}: Cause with title {doc['cause']} wasn't found in dictionary")
+
+        return None
+
+    def run_test(self):
+        mock_collection = self.run_test_data(
+            [
+                {
+                    "_id": "39e5353444754b2fbe42bf0282ac9510",
+                    "_rev": "1-b2e0f794769c490bacdb8053df816d10",
+                    "cause": "technicalReasons",
+                    "causeDescription": "причина",
+                    "causeDescription_en": "cause",
+                    "procurementMethodType": "negotiation",
+                    "revisions": [
+                        {
+                            "author": "broker",
+                            "changes": [{"op": "remove", "path": "/causeDescription"}],
+                            "rev": None,
+                            "date": "2025-04-23T20:01:57.079339+03:00",
+                        },
+                    ],
+                },
+                {
+                    "_id": "39e5353444754b2fbe42bf0282ac9511",
+                    "_rev": "1-b2e0f794769c490bacdb8053df816d11",
+                    "cause": "lastHope",
+                    "causeDescription": "причина",
+                    "procurementMethodType": "negotiation.quick",
+                    "revisions": [
+                        {
+                            "author": "broker",
+                            "changes": [{"op": "remove", "path": "/causeDescription"}],
+                            "rev": None,
+                            "date": "2025-04-23T20:01:57.079339+03:00",
+                        },
+                    ],
+                },
+                {
+                    "_id": "39e5353444754b2fbe42bf0282ac9512",
+                    "_rev": "1-b2e0f794769c490bacdb8053df816d12",
+                    "cause": "hematopoieticStemCells",
+                    "procurementMethodType": "reporting",
+                    "revisions": [
+                        {
+                            "author": "broker",
+                            "changes": [{"op": "remove", "path": "/causeDescription"}],
+                            "rev": None,
+                            "date": "2025-04-23T20:01:57.079339+03:00",
+                        },
+                    ],
+                },
+                {
+                    "_id": "39e5353444754b2fbe42bf0282ac9513",
+                    "_rev": "1-b2e0f794769c490bacdb8053df816d13",
+                    "cause": "defenceVehicles",
+                    "causeDescription": "причина",
+                    "procurementMethodType": "reporting",
+                    "revisions": [
+                        {
+                            "author": "broker",
+                            "changes": [{"op": "remove", "path": "/causeDescription"}],
+                            "rev": None,
+                            "date": "2025-04-23T20:01:57.079339+03:00",
+                        },
+                    ],
+                },
+            ],
+        )
+
+        mock_collection.bulk_write.assert_called_once_with(
+            [
+                UpdateOne(
+                    {
+                        "_id": "39e5353444754b2fbe42bf0282ac9510",
+                        "_rev": "1-b2e0f794769c490bacdb8053df816d10",
+                    },
+                    [
+                        {
+                            "$set": {
+                                "_id": "39e5353444754b2fbe42bf0282ac9510",
+                                "_rev": "1-b2e0f794769c490bacdb8053df816d10",
+                                "cause": "technicalReasons",
+                                "causeDescription": "причина",
+                                "causeDescription_en": "cause",
+                                "procurementMethodType": "negotiation",
+                                "causeDetails": {
+                                    "title": "technicalReasons",
+                                    "scheme": "LAW922",
+                                    "description": "причина",
+                                    "description_en": "cause",
+                                },
+                                "revisions": [
+                                    {
+                                        "author": "broker",
+                                        "changes": [{"op": "remove", "path": "/causeDescription"}],
+                                        "rev": None,
+                                        "date": "2025-04-23T20:01:57.079339+03:00",
+                                    },
+                                    {
+                                        "author": "migration",
+                                        "changes": [{"op": "remove", "path": "/causeDetails"}],
+                                        "rev": ANY,
+                                        "date": ANY,
+                                    },
+                                ],
+                            }
+                        },
+                        {
+                            "$set": {
+                                "_rev": ANY,
+                            }
+                        },
+                        {
+                            "$set": {
+                                "public_modified": {"$divide": [{"$toLong": "$$NOW"}, 1000]},
+                                "public_ts": "$$CLUSTER_TIME",
+                            }
+                        },
+                    ],
+                ),
+                UpdateOne(
+                    {
+                        "_id": "39e5353444754b2fbe42bf0282ac9511",
+                        "_rev": "1-b2e0f794769c490bacdb8053df816d11",
+                    },
+                    [
+                        {
+                            "$set": {
+                                "_id": "39e5353444754b2fbe42bf0282ac9511",
+                                "_rev": "1-b2e0f794769c490bacdb8053df816d11",
+                                "cause": "lastHope",
+                                "causeDescription": "причина",
+                                "procurementMethodType": "negotiation.quick",
+                                "causeDetails": {
+                                    "title": "lastHope",
+                                    "scheme": "LAW922",
+                                    "description": "причина",
+                                },
+                                "revisions": [
+                                    {
+                                        "author": "broker",
+                                        "changes": [{"op": "remove", "path": "/causeDescription"}],
+                                        "rev": None,
+                                        "date": "2025-04-23T20:01:57.079339+03:00",
+                                    },
+                                    {
+                                        "author": "migration",
+                                        "changes": [{"op": "remove", "path": "/causeDetails"}],
+                                        "rev": ANY,
+                                        "date": ANY,
+                                    },
+                                ],
+                            }
+                        },
+                        {
+                            "$set": {
+                                "_rev": ANY,
+                            }
+                        },
+                        {
+                            "$set": {
+                                "public_modified": {"$divide": [{"$toLong": "$$NOW"}, 1000]},
+                                "public_ts": "$$CLUSTER_TIME",
+                            }
+                        },
+                    ],
+                ),
+                UpdateOne(
+                    {
+                        "_id": "39e5353444754b2fbe42bf0282ac9512",
+                        "_rev": "1-b2e0f794769c490bacdb8053df816d12",
+                    },
+                    [
+                        {
+                            "$set": {
+                                "_id": "39e5353444754b2fbe42bf0282ac9512",
+                                "_rev": "1-b2e0f794769c490bacdb8053df816d12",
+                                "cause": "hematopoieticStemCells",
+                                "procurementMethodType": "reporting",
+                                "causeDetails": {
+                                    "title": "hematopoieticStemCells",
+                                    "scheme": "LAW922",
+                                },
+                                "revisions": [
+                                    {
+                                        "author": "broker",
+                                        "changes": [{"op": "remove", "path": "/causeDescription"}],
+                                        "rev": None,
+                                        "date": "2025-04-23T20:01:57.079339+03:00",
+                                    },
+                                    {
+                                        "author": "migration",
+                                        "changes": [{"op": "remove", "path": "/causeDetails"}],
+                                        "rev": ANY,
+                                        "date": ANY,
+                                    },
+                                ],
+                            }
+                        },
+                        {
+                            "$set": {
+                                "_rev": ANY,
+                            }
+                        },
+                        {
+                            "$set": {
+                                "public_modified": {"$divide": [{"$toLong": "$$NOW"}, 1000]},
+                                "public_ts": "$$CLUSTER_TIME",
+                            }
+                        },
+                    ],
+                ),
+                UpdateOne(
+                    {
+                        "_id": "39e5353444754b2fbe42bf0282ac9513",
+                        "_rev": "1-b2e0f794769c490bacdb8053df816d13",
+                    },
+                    [
+                        {
+                            "$set": {
+                                "_id": "39e5353444754b2fbe42bf0282ac9513",
+                                "_rev": "1-b2e0f794769c490bacdb8053df816d13",
+                                "cause": "defenceVehicles",
+                                "causeDescription": "причина",
+                                "procurementMethodType": "reporting",
+                                "causeDetails": {
+                                    "title": "defenceVehicles",
+                                    "scheme": "DECREE1178",
+                                    "description": "причина",
+                                },
+                                "revisions": [
+                                    {
+                                        "author": "broker",
+                                        "changes": [{"op": "remove", "path": "/causeDescription"}],
+                                        "rev": None,
+                                        "date": "2025-04-23T20:01:57.079339+03:00",
+                                    },
+                                    {
+                                        "author": "migration",
+                                        "changes": [{"op": "remove", "path": "/causeDetails"}],
+                                        "rev": ANY,
+                                        "date": ANY,
+                                    },
+                                ],
+                            }
+                        },
+                        {
+                            "$set": {
+                                "_rev": ANY,
+                            }
+                        },
+                        {
+                            "$set": {
+                                "public_modified": {"$divide": [{"$toLong": "$$NOW"}, 1000]},
+                                "public_ts": "$$CLUSTER_TIME",
+                            }
+                        },
+                    ],
+                ),
+            ]
+        )
+
+
+if __name__ == "__main__":
+    migrate_collection(Migration)
---
+++ src/openprocurement/tender/limited/procedure/models/cause.py
@@ -0,0 +1,55 @@
+from enum import StrEnum
+
+from schematics.types import StringType
+
+from openprocurement.api.constants import TENDER_CAUSE_DECREE_1178, TENDER_CAUSE_LAW_922
+from openprocurement.api.procedure.models.base import Model
+from openprocurement.api.validation import ValidationError
+
+
+class CauseScheme(StrEnum):
+    DECREE_1178 = "DECREE1178"
+    LAW_922 = "LAW922"
+
+
+CAUSE_SCHEME_MAPPING = {
+    CauseScheme.DECREE_1178.value: TENDER_CAUSE_DECREE_1178,
+    CauseScheme.LAW_922.value: TENDER_CAUSE_LAW_922,
+}
+
+
+class CauseDetails(Model):
+    title = StringType(required=True)
+    scheme = StringType(
+        required=True,
+        choices=[
+            CauseScheme.DECREE_1178.value,
+            CauseScheme.LAW_922.value,
+        ],
+    )
+    description = StringType()
+    description_en = StringType()
+
+    def validate_title(self, data, value):
+        if value is not None and value not in CAUSE_SCHEME_MAPPING[data["scheme"]]:
+            raise ValidationError(f"Value must be one of {CAUSE_SCHEME_MAPPING[data['scheme']]}.")
+
+
+class NegotiationCauseDetails(CauseDetails):
+    def validate_title(self, data, value):
+        from openprocurement.tender.limited.procedure.models.tender import (
+            validate_cause,
+        )
+
+        validate_cause(value)
+        super().validate_title(self, data, value)
+
+
+class NegotiationQuickCauseDetails(CauseDetails):
+    def validate_title(self, data, value):
+        from openprocurement.tender.limited.procedure.models.tender import (
+            validate_cause_quick,
+        )
+
+        validate_cause_quick(value)
+        super().validate_title(self, data, value)
