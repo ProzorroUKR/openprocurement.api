06dc1910e11855ab881d9be5078fe4fc79b1bb4c
mhoang <hoang.viet.ha@raccoongang.com>
2026-02-24 12:38:30 +0200

    fix: fixed auction data conversion

--- src/openprocurement/tender/arma/procedure/views/auction.py
+++ src/openprocurement/tender/arma/procedure/views/auction.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 from cornice.resource import resource
 
 from openprocurement.api.procedure.validation import validate_input_data
@@ -24,54 +26,47 @@ from openprocurement.tender.core.procedure.views.auction import TenderAuctionRes
 )
 class AuctionResource(TenderAuctionResource):
     state_class = TenderState
-    patch_value_field_names = {"value", "initialValue", "weightedValue"}
+    patch_value_field_names = {"value", "initialValue", "weightedValue", "minimalStep"}
 
-    def patch_from_auction_data(self, data):
+    @staticmethod
+    def convert_value_from_auction(value: dict):
         """
-        Method patches auction results to fit ARMA structure
+        Method converts data to fit tender value format
         """
+        value.pop("currency", None)
+        value.pop("valueAddedTaxIncluded", None)
+        amount = value.pop("amount", None)
+        if amount is not None:
+            value["amountPercentage"] = amount
 
-        def _patch_value(value):
-            value.pop("currency", None)
-            value.pop("valueAddedTaxIncluded", None)
-            amount = value.pop("amount", None)
-            if amount is not None:
-                value["amountPercentage"] = amount
+    @staticmethod
+    def convert_value_to_auction(value: dict):
+        """
+        Method converts data to fit auction value format
+        """
+        value["currency"] = "%"
+        amount_percentage = value.pop("amountPercentage", None)
+        if amount_percentage is not None:
+            value["amount"] = amount_percentage
 
-        for bid in data["bids"]:
+    def convert_value_data(self, data: dict, patch_value_callable: Callable) -> dict:
+        """
+        Method converts data to fit expected structure
+        """
+        for bid in data.get("bids", []):
             for value_field in self.patch_value_field_names:
                 if val := bid.get(value_field):
-                    _patch_value(val)
+                    patch_value_callable(val)
 
-            if "lotValues" in bid:
-                for l_value in bid["lotValues"]:
-                    for value_field in self.patch_value_field_names:
-                        if val := l_value.get(value_field):
-                            _patch_value(val)
+            for l_value in bid.get("lotValues", []):
+                for value_field in self.patch_value_field_names:
+                    if val := l_value.get(value_field):
+                        patch_value_callable(val)
 
-        return data
-
-    def patch_to_auction_data(self, data):
-        """
-        Method patches ARMA structure to fit auction structure
-        """
-
-        def _patch_value(value):
-            value["currency"] = "%"
-            amount_percentage = value.pop("amountPercentage", None)
-            if amount_percentage is not None:
-                value["amount"] = amount_percentage
-
-        for bid in data["data"]["bids"]:
+        for lot in data.get("lots", []):
             for value_field in self.patch_value_field_names:
-                if val := bid.get(value_field):
-                    _patch_value(val)
-
-            if "lotValues" in bid:
-                for l_value in bid["lotValues"]:
-                    for value_field in self.patch_value_field_names:
-                        if val := l_value.get(value_field):
-                            _patch_value(val)
+                if val := lot.get(value_field):
+                    patch_value_callable(val)
 
         return data
 
@@ -83,7 +78,9 @@ class AuctionResource(TenderAuctionResource):
         ),
     )
     def collection_post(self):
-        self.request.validated["data"] = self.patch_from_auction_data(self.request.validated["data"])
+        self.request.validated["data"] = self.convert_value_data(
+            self.request.validated["data"], self.convert_value_from_auction
+        )
         return super().collection_post()
 
     @json_view(
@@ -95,7 +92,9 @@ class AuctionResource(TenderAuctionResource):
         ),
     )
     def post(self):
-        self.request.validated["data"] = self.patch_from_auction_data(self.request.validated["data"])
+        self.request.validated["data"] = self.convert_value_data(
+            self.request.validated["data"], self.convert_value_from_auction
+        )
         return super().post()
 
     @json_view(
@@ -103,4 +102,6 @@ class AuctionResource(TenderAuctionResource):
         validators=(validate_auction_tender_status,),
     )
     def collection_get(self):
-        return self.patch_to_auction_data(super().collection_get())
+        res = super().collection_get()
+        res["data"] = self.convert_value_data(res["data"], self.convert_value_to_auction)
+        return res
