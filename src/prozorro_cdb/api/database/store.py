c07153b3a6a884c36eab3b9bf3d1dda4e00bca13
smithumble <smithumble@gmail.com>
2026-03-11 18:42:00 +0200

    Format and comment on index definitions

--- src/prozorro_cdb/api/database/store.py
+++ src/prozorro_cdb/api/database/store.py
@@ -273,42 +273,51 @@ class BaseCollection:
         self.collection = getattr(store.database, self.collection_name)
 
     def get_indexes(self) -> list[IndexModel]:
-        # Making multiple indexes with the same unique key is supposed to be impossible
-        # https://jira.mongodb.org/browse/SERVER-25023
-        # and https://docs.mongodb.com/manual/core/index-partial/#restrictions
-        # ``In MongoDB, you cannot create multiple versions of an index that differ only in the options.
-        #   As such, you cannot create multiple partial indexes that differ only by the filter expression.``
-        # Hold my 🍺
-        test_by_public_modified = IndexModel(
-            [("public_modified", ASCENDING), ("existing_key", ASCENDING)],
-            name="test_by_public_modified",
-            partialFilterExpression={
-                "is_test": True,
-                "is_public": True,
-            },
-        )
-        real_by_public_modified = IndexModel(
-            [("public_modified", ASCENDING)],
+        real_by_public_modified_index = IndexModel(
+            [
+                ("public_modified", ASCENDING),
+            ],
             name="real_by_public_modified",
             partialFilterExpression={
                 "is_test": False,
                 "is_public": True,
             },
         )
-        all_by_public_modified = IndexModel(
+        test_by_public_modified_index = IndexModel(
+            [
+                ("public_modified", ASCENDING),
+                ("existing_key", ASCENDING),
+                # this hack key was used to allow index with the same fields
+                # but different partial index filters
+                # https://jira.mongodb.org/browse/SERVER-25023
+                # this is not required anymore
+                # but we keep it so we don't need to recreate the existing indexes
+            ],
+            name="test_by_public_modified",
+            partialFilterExpression={
+                "is_test": True,
+                "is_public": True,
+            },
+        )
+        all_by_public_modified_index = IndexModel(
             [
                 ("public_modified", ASCENDING),
                 ("surely_existing_key", ASCENDING),
-            ],  # makes key unique https://jira.mongodb.org/browse/SERVER-25023
+                # this hack key was used to allow index with the same fields
+                # but different partial index filters
+                # https://jira.mongodb.org/browse/SERVER-25023
+                # this is not required anymore
+                # but we keep it so we don't need to recreate the existing indexes
+            ],
             name="all_by_public_modified",
             partialFilterExpression={
                 "is_public": True,
             },
         )
         return [
-            test_by_public_modified,
-            real_by_public_modified,
-            all_by_public_modified,
+            real_by_public_modified_index,
+            test_by_public_modified_index,
+            all_by_public_modified_index,
         ]
 
     async def create_indexes(self) -> None:
